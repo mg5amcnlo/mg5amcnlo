2938c4aae24fb1f2edab56cb7ac11132209c6473
Michel Herquet <herquet@gmail.com>
2009-09-25 16:34:29 +0200

    working on cmd interface

---
+++ packages/interface/cmd_interface.py
@@ -0,0 +1,32 @@
+##############################################################################
+#
+# Copyright (c) 2009 The MadGraph Development team and Contributors
+#
+# This file is a part of the MadGraph 5 project, an application which 
+# automatically generates Feynman diagrams and matrix elements for arbitrary
+# high-energy processes in the Standard Model and beyond.
+#
+# It is subject to the MadGraph license which should accompany this 
+# distribution.
+#
+# For more information, please visit: http://madgraph.phys.ucl.ac.be
+#
+##############################################################################
+
+"""A user friendly command line interface to access MadGraph features."""
+
+import cmd
+
+class MadGraphCmd(cmd.Cmd):
+    """The command line processor of MadGraph"""
+
+    prompt = 'mg>'
+
+    intro = """adjksdhas
+    dsjadlkajs"""
+
+    def do_EOF(self, line):
+        return True
+
+if __name__ == '__main__':
+    MadGraphCmd().cmdloop()
