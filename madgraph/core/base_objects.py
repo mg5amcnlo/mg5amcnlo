84786f946a71dec63ce252f35b25a3b8696cb73d
Johan Alwall <alwall@alwall-laptop>
2009-11-07 12:26:33 +0900

    Fixed bug in diagram generation due to fermion flow

--- madgraph/core/base_objects.py
+++ madgraph/core/base_objects.py
@@ -19,6 +19,7 @@ interaction, model, leg, vertex, process, ..."""
 import copy
 import logging
 import re
+import itertools
 
 #===============================================================================
 # PhysicsObject
@@ -453,62 +454,33 @@ class Interaction(PhysicsObject):
 
         return ['id', 'particles', 'color', 'lorentz', 'couplings', 'orders']
 
-    def __permutate(self, seq):
-        """Permutate a sequence and return a list of all permutations"""
-        if not seq:
-            return [seq] # is an empty sequence
-        else:
-            temp = []
-            for k in range(len(seq)):
-                part = seq[:k] + seq[k + 1:]
-                for m in self.__permutate(part):
-                    temp.append(seq[k:k + 1] + m)
-            return temp
-
     def generate_dict_entries(self, ref_dict_to0, ref_dict_to1):
         """Add entries corresponding to the current interactions to 
         the reference dictionaries (for n>0 and n-1>1)"""
 
         # Create n>0 entries. Format is (p1,p2,p3,...):interaction_id.
-        for permut in self.__permutate(self['particles']):
+        for permut in itertools.permutations(self['particles'],len(self['particles'])):
             pdg_tuple = tuple([p.get_pdg_code() for p in permut])
             if pdg_tuple not in ref_dict_to0.keys():
                 ref_dict_to0[pdg_tuple] = self['id']
-            pdg_tuple = tuple([p.get_anti_pdg_code() for p in permut])
-            if pdg_tuple not in ref_dict_to0.keys():
-                ref_dict_to0[pdg_tuple] = self['id']
 
-        # Create n-1>1 entries. Note that, in the n-1
-        # > 1 dictionary, the 1 entry should have opposite sign as
-        # compared to the n > 0 dictionary, since this should replace
-        # the n-1 particles. We prefer to keep track of the sign
-        # (part/antipart) here rather than in the diagram generation.
+        # Create n-1>1 entries. Note that, in the n-1 > 1 dictionary,
+        # the n-1 entries should have opposite sign as compared to
+        # interaction, since the interaction has outgoing particles,
+        # while in the dictionary we treat the n-1 particles as
+        # incoming
 
-        for part in self['particles']:
-
-            # Create a list w/o part
-            short_part_list = copy.copy(self['particles'])
-            short_part_list.remove(part)
-
-            # Add all permutations
-            for permut in self.__permutate(short_part_list):
-                # Add interaction permutation
-                pdg_tuple = tuple([p.get_pdg_code() for p in permut])
-                pdg_part = part.get_anti_pdg_code()
-                if pdg_tuple in ref_dict_to1.keys():
-                    if (pdg_part, self['id']) not in  ref_dict_to1[pdg_tuple]:
-                        ref_dict_to1[pdg_tuple].append((pdg_part, self['id']))
+        for cycl_perm in cyclic_permutations(self['particles'],
+                                             len(self['particles'])-1):
+            pdg_tuple = tuple([p.get_anti_pdg_code() for p in cycl_perm[0]])
+            pdg_part = cycl_perm[1][0].get_pdg_code()
+            for perm in itertools.permutations(pdg_tuple,len(pdg_tuple)):
+                if perm in ref_dict_to1.keys():
+                    if (pdg_part, self['id']) not in  ref_dict_to1[perm]:
+                        ref_dict_to1[perm].append((pdg_part, self['id']))
                 else:
-                    ref_dict_to1[pdg_tuple] = [(pdg_part, self['id'])]
-                # Add hermitian conjugate
-                pdg_tuple = tuple([p.get_anti_pdg_code() for p in permut])
-                pdg_part = part.get_pdg_code()
-                if pdg_tuple in ref_dict_to1.keys():
-                    if (pdg_part, self['id']) not in  ref_dict_to1[pdg_tuple]:
-                        ref_dict_to1[pdg_tuple].append((pdg_part, self['id']))
-                else:
-                    ref_dict_to1[pdg_tuple] = [(pdg_part, self['id'])]
-
+                    ref_dict_to1[perm] = [(pdg_part, self['id'])]
+            
 
 #===============================================================================
 # InteractionList
@@ -931,3 +903,41 @@ class ProcessList(PhysicsObjectList):
         
         return isinstance(obj, Process)
 
+#===============================================================================
+# Global helper methods
+#===============================================================================
+
+def cyclic_permutations(mylist,n):
+    """Returns all cyclic permutations with n elements in mylist, and
+    the rests. For 2-perm in [1,2,3]:
+    [ [[1,2],[3]] , [[2,3],[1]] , [[3,1],[2]] ]
+    """
+
+    if not isinstance(mylist,list) and not isinstance(mylist,tuple):
+        raise PermutationsError, \
+              "Argument %s is not a list" % repr(mylist)
+
+    if not isinstance(n,int):
+        raise PermutationsError, \
+              "Argument n is not an integer"
+
+    if n < 1:
+        return []
+
+    # Create a new list which is [mylist,mylist], then pick all unique
+    # series of elements from this list
+    double_list = copy.copy(mylist)
+    double_list.extend(mylist)
+
+    master_list = []
+    i = 0
+    iterlist = double_list[0:n]
+    rest = mylist[n:]
+    while not [iterlist,rest] in master_list:
+        master_list.append([iterlist,rest])
+        i = i + 1
+        iterlist = double_list[i:i + n]
+        rest = mylist[i+n:]
+        rest.extend(mylist[max(0,i+n-len(mylist)):i])
+
+    return master_list
