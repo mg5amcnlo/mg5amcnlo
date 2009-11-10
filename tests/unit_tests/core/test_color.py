8a28179df4bb10a817ce91f4ea6e6e2032c63e49
Michel Herquet <herquet@gmail.com>
2009-11-10 16:50:28 +0100

    Added empty modules for color

---
+++ tests/unit_tests/core/test_color.py
@@ -0,0 +1,27 @@
+################################################################################
+#
+# Copyright (c) 2009 The MadGraph Development team and Contributors
+#
+# This file is a part of the MadGraph 5 project, an application which 
+# automatically generates Feynman diagrams and matrix elements for arbitrary
+# high-energy processes in the Standard Model and beyond.
+#
+# It is subject to the MadGraph license which should accompany this 
+# distribution.
+#
+# For more information, please visit: http://madgraph.phys.ucl.ac.be
+#
+################################################################################
+
+"""Unit test library for the color related routines in the core library"""
+
+import unittest
+
+import madgraph.core.color as color
+
+class ColorTest(unittest.TestCase):
+    """Test class for code related to color"""
+
+    def setUp(self):
+        pass
+
