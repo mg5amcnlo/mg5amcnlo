a8aa15730ec893a18663d2af11cf8efbb04ebd18
Will Link <will.j.link@gmail.com>
2010-07-23 15:26:57 -0500

    Functions needed for helas wavefunctions

---
+++ aloha/Template/need.cc
@@ -0,0 +1,11 @@
+double Min(double a,double b){
+return (a>b)?b:a;}
+
+double Max(double a,double b){ 
+return (a>b)?a:b;}
+
+double Sgn(double a,double b){ 
+return (b<0)?-a:a;}
+
+double Abs(double a){
+return (a<0)?-a:a;}
---
+++ aloha/Template/need.h
@@ -0,0 +1,7 @@
+#ifndef need_guard
+#define need_guard
+double Min(double a,double b);
+double Max(double c,double d);
+double Sgn(double e,double f);
+double Abs(double g);
+#endif
