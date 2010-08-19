a4b0a28bf65d004f23cc51ebbc376ed11defb5a1
Johan Alwall <alwall@alwall-laptop>
2010-08-19 17:06:28 +0800

    Fixed function library for mssm as well

--- models/sm/function_library.py
+++ models/sm/function_library.py
@@ -11,7 +11,7 @@
 __date__ = "22 July 2010"
 __author__ = "claude.duhr@durham.ac.uk"
 
-from cmath import cos, sin, acos, asin
+import cmath
 from object_library import all_functions, Function
 
 #
@@ -31,24 +31,23 @@ im = Function(name = 'im',
               arguments = ('z',),
               expression = 'z.imag')
 
-
 # New functions (trigonometric)
 
 sec = Function(name = 'sec',
              arguments = ('z',),
-             expression = '1./cos(z)')
+             expression = '1./cmath.cos(z)')
 
 asec = Function(name = 'asec',
              arguments = ('z',),
-             expression = 'acos(1./z)')
+             expression = 'cmath.acos(1./z)')
 
 csc = Function(name = 'csc',
              arguments = ('z',),
-             expression = '1./sin(z)')
+             expression = '1./cmath.sin(z)')
 
 acsc = Function(name = 'acsc',
              arguments = ('z',),
-             expression = 'asin(1./z)')
+             expression = 'cmath.asin(1./z)')
 
 
 
