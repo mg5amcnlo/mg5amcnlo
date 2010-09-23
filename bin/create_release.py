5f47a9f80fb0558e2b644378647c3de35486d763
Johan Alwall <alwall@alwall-laptop>
2010-09-23 20:41:20 +0800

    Added file bin/create_release.py for creating a release (which includes a MadEvent Template file)

---
+++ bin/create_release.py
@@ -0,0 +1,205 @@
+#! /usr/bin/env python
+
+################################################################################
+#
+# Copyright (c) 2009 The MadGraph Development team and Contributors
+#
+# This file is a part of the MadGraph 5 project, an application which 
+# automatically generates Feynman diagrams and matrix elements for arbitrary
+# high-energy processes in the Standard Model and beyond.
+#
+# It is subject to the MadGraph license which should accompany this 
+# distribution.
+#
+# For more information, please visit: http://madgraph.phys.ucl.ac.be
+#
+################################################################################
+
+"""This is a simple script to create a release for MadGraph 5, based
+on the latest Bazaar commit of the present version. It performs the
+following actions:
+
+1. bzr branch the present directory to a new directory
+   MadGraph5_vVERSION
+
+2. Copy the Template either from the present directory or from a valid
+   MG_ME directory in the path or given by the -d flag, and remove the
+   bin/newprocess file
+
+   *WARNING* Note that it is your responsibility to make sure this
+   Template is up-to-date!!
+
+
+3. Copy all v4 model directories from the present directory and/or
+   from an MG_ME directory as specified in point 2. to the models
+   directory (with names modelname_v4)
+
+4. Create the automatic documentation in the apidoc directory
+
+5. Remove the .bzr directory
+
+6. tar the MadGraph5_vVERSION directory.
+"""
+
+import sys
+
+if not sys.version_info[0] == 2 or sys.version_info[1] < 6:
+    sys.exit('MadGraph 5 works only with python 2.6 or later (but not python 3.X).\n\
+               Please upgrate your version of python.')
+
+import glob
+import optparse
+import logging
+import logging.config
+
+import os
+import os.path as path
+import shutil
+import subprocess
+
+# Get the parent directory (mg root) of the script real path (bin)
+# and add it to the current PYTHONPATH
+
+root_path = path.split(path.dirname(path.realpath( __file__ )))[0]
+sys.path.append(root_path)
+
+print root_path
+
+import madgraph.iolibs.misc as misc
+from madgraph import MG4DIR, MG5DIR
+
+
+# Write out nice usage message if called with -h or --help
+usage = "usage: %prog [options] [FILE] "
+parser = optparse.OptionParser(usage=usage)
+parser.add_option("-l", "--logging", default='INFO',
+                  help="logging level (DEBUG|INFO|WARNING|ERROR|CRITICAL) [%default]")
+parser.add_option("-d", "--mgme_dir", default='', dest = 'mgme_dir',
+                  help="Use MG_ME directory MGME_DIR")
+(options, args) = parser.parse_args()
+if len(args) == 0:
+    args = ''
+
+# Set logging level according to the logging level given by options
+logging.basicConfig(level=vars(logging)[options.logging])
+
+# If an MG_ME dir is given in the options, use that dir for Template
+# and v4 models
+
+mgme_dir = options.mgme_dir
+
+# Set Template directory path
+
+if mgme_dir and path.isdir(path.join(mgme_dir, 'Template')):
+    template_path = path.join(mgme_dir, 'Template')
+
+elif path.isdir(path.join(MG5DIR, 'Template')):
+    template_path = path.join(MG5DIR, 'Template')
+
+elif MG4DIR and path.isdir(path.join(MG4DIR, 'Template')):
+    template_path = path.join(MG4DIR, 'Template')
+else:
+    logging.error("Could not find the Template directory in the path")
+    exit()
+
+
+    
+# 1. bzr branch the present directory to a new directory
+#    MadGraph5_vVERSION
+
+filepath = "MadGraph5_v" + misc.get_pkg_info()['version']
+
+logging.info("Branching " + MG5DIR + " to directory " + filepath)
+status = subprocess.call(['bzr', 'branch', MG5DIR, filepath])
+if status == 3:
+    logging.error("Please run rm -rf "+filepath)
+    exit()
+elif status:
+    logging.error("Script stopped")
+    exit()
+
+# 2. Copy the Template either from the present directory or from a valid
+#    MG_ME directory in the path or given by the -d flag, and remove the
+#    bin/newprocess file
+
+logging.info("Copying " + template_path)
+shutil.copytree(template_path, path.join(filepath, 'Template'), symlinks = True)
+os.remove(path.join(filepath, 'Template', 'bin', 'newprocess'))
+
+for i in range(6):
+    cvs_dirs = glob.glob(path.join(filepath,
+                                   path.join('Template', *(['*']*i)), 'CVS'))
+    if not cvs_dirs:
+        break
+    for cvs_dir in cvs_dirs:
+        shutil.rmtree(cvs_dir)
+
+# 3. Copy all v4 model directories from the present directory and/or
+#    from an MG_ME directory as specified in point 2. to the models
+#    directory (with names modelname_v4)
+
+model_path = ""
+if mgme_dir:
+    if path.join(mgme_dir, 'Models'):
+        model_path = path.join(mgme_dir, 'Models')
+elif MG4DIR and path.join(MG4DIR, 'Models'):
+    model_path = path.join(MG4DIR, 'Models')
+
+if model_path:
+    logging.info("Copying v4 models from " + model_path + ":")
+    for mdir in [d for d in glob.glob(path.join(model_path, "*")) \
+                 if path.isdir(d) and \
+                 path.exists(path.join(d, "particles.dat"))]:
+        modelname = path.split(mdir)[-1]
+        new_m_path = path.join(filepath, 'models', modelname + "_v4")
+        logging.info(mdir + " -> " + new_m_path)
+        shutil.copytree(mdir, new_m_path)
+        if path.exists(path.join(new_m_path, "model.pkl")):
+            os.remove(path.join(new_m_path, "model.pkl"))
+
+v4_models = [d for d in glob.glob(path.join(MG5DIR, "models", "*_v4")) \
+             if path.isdir(d) and \
+             path.exists(path.join(d, "particles.dat"))]
+
+if v4_models:
+    logging.info("Copying v4 models from " + path.join(MG5DIR, "models") + ":")
+    for mdir in v4_models:
+        modelname = path.split(mdir)[-1]
+        new_m_path = path.join(filepath, 'models', modelname + "_v4")
+        logging.info(mdir + " -> " + new_m_path)
+        shutil.copytree(mdir, new_m_path)
+        if path.exists(path.join(new_m_path, "model.pkl")):
+            os.remove(path.join(new_m_path, "model.pkl"))
+
+# 4. Create the automatic documentation in the apidoc directory
+
+status1 = subprocess.call(['epydoc', '--html', '-o', 'apidoc', 'madgraph',
+                          os.path.join('models', '*.py')], cwd = filepath)
+
+if status1:
+    info.warning('Non-0 exit code %d from epydoc. Please check output.' % \
+                 status)
+
+# 5. Remove the .bzr directory and the create_release.py file,
+#    take care of README files.
+
+shutil.rmtree(path.join(filepath, '.bzr'))
+os.remove(path.join(filepath, 'bin', 'create_release.py'))
+os.remove(path.join(filepath, 'README.developer'))
+shutil.move(path.join(filepath, 'README.release'), path.join(filepath, 'README'))
+
+# 6. tar the MadGraph5_vVERSION directory.
+
+logging.info("Create the tar file " + filepath + ".tar.gz")
+
+status2 = subprocess.call(['tar', 'czf', filepath + ".tar.gz", filepath])
+
+if status2:
+    info.warning('Non-0 exit code %d from tar. Please check result.' % \
+                 status)
+
+if not status1 and not status2:
+    # Remove the MadGraph5_vVERSION directory
+    logging.info("Removing directory " + filepath)
+    shutil.rmtree(filepath)
+    
