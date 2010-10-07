3bed86dca01fbd435558bd6d6bfecca052d204a4
Johan Alwall <alwall@alwall-laptop>
2010-10-07 08:08:46 +0800

    Added mention of automatic coupling orders in help_generate and tutorial

--- madgraph/interface/tutorial_text.py
+++ madgraph/interface/tutorial_text.py
@@ -28,13 +28,14 @@ a) How to generate a process
 b) How to create output for MadEvent
 
 Let's start with the first point, how to generate a process:
-mg5>generate p p > t t~ QED=0
+mg5>generate p p > t t~
 Note that a space is mandatory between the particle names.
 """
 
 generate = """
 You have just generated a new process.
-
+Note that the coupling order \"QED=0\" was automatically added by MG5
+to avoid non-QCD diagrams which have negligible contribution.
 You can find more information on supported syntax by using:
 mg5>help generate
 To list all defined processes, type
@@ -46,7 +47,7 @@ mg5>display particles
 mg5>display multiparticles
 
 If you want to add a second process, use the add process command:
-mg5>add process p p > W+ j QED=1, W+ > l+ vl @2
+mg5>add process p p > W+ j, W+ > l+ vl @2
 This adds a decay chain process, with the W+ decaying
 leptonically.
 
