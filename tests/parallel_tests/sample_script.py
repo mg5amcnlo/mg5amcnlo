871675acea95d274ba48b2481cc3bae37ed14996
Johan Alwall <alwall@alwall-laptop>
2010-10-20 08:37:58 -0500

    Fix problem when the HOME environment variable is not available

--- tests/parallel_tests/sample_script.py
+++ tests/parallel_tests/sample_script.py
@@ -54,6 +54,9 @@ if '__main__' == __name__:
                     'e+ x1+ > e+ h1 x1+','e+ x1+ > e+ h2 x1+','e+ x1+ > e+ h3 x1+']
     my_proc_list += ['el+ h2 > el+ w+ w-']
                    
+    my_proc_list = me_comparator.create_proc_list(['g', 'go'], initial=2,
+                                                  final=2)
+
     # Create a MERunner object for MG4
     my_mg4 = me_comparator.MG4Runner()
     my_mg4.setup(mg4_path)
