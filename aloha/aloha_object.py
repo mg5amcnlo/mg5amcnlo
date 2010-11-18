bfa9047547f8fe2e60739463379b0afda7c7622e
Olivier Mattelaer <olivier.mattelaer@uclouvain.be>
2010-11-18 15:21:11 +0100

    add unittest for the Epsilon object of ALOHA. modify Epsilon object and the equality between object such that the new test passes.

--- aloha/aloha_object.py
+++ aloha/aloha_object.py
@@ -439,7 +439,8 @@ def give_sign_perm(perm0, perm1):
     and have the same elements. No need to check for these
     conditions.
     """
-
+    assert len(perm0) == len(perm1) 
+        
     perm1 = list(perm1) ## copy this into a list so we don't mutate the original
     perm1_map = dict((v, i) for i,v in enumerate(perm1))
 
@@ -449,30 +450,41 @@ def give_sign_perm(perm0, perm1):
         if p0 != p1:
             sloc = perm1_map[p0]                       # Find position in perm1
             perm1[loc], perm1[sloc] = p0, p1           # Swap in perm1
-            perm1_map[p0], perm1_map[p1] = sloc, loc   # Swap the map
+            perm1_map[p0], perm1_map[p1] = loc, sloc   # Swap the map
             transCount += 1
-    # Even number of transpositions means equal parity
+            
+    # Even number of transposition means equal parity
     return -2 * (transCount % 2) + 1
     
 # Practical definition of Epsilon
 class Epsilon(aloha_lib.LorentzObject):
     """ The fully anti-symmetric object in Lorentz-Space """
+ 
+    def give_parity(self, perm):
+        """return the parity of the permutation"""
+        assert set(perm) == set([0,1,2,3]) 
+        
+        i1 , i2, i3, i4 = perm
+        #formula found on wikipedia
+        return ((i2-i1) * (i3-i1) *(i4-i1) * (i3-i2) * (i4-i2) *(i4-i3))/12 
    
     # DEFINE THE REPRESENTATION OF EPSILON
            
     def __init__(self, lorentz1, lorentz2, lorentz3, lorentz4, prefactor=1):
        
        lorentz_list = [lorentz1 , lorentz2, lorentz3, lorentz4]
-       order_lor = list(lorentz_list).sort()
-       sign = self.give_sign_perm(order_lor, lorentz_list)
+       order_lor = list(lorentz_list)
+       order_lor.sort()
        
-       aloha_lib.LorentzObject.__init__(self, lorentz_list, \
+       sign = give_sign_perm(order_lor, lorentz_list)
+       
+       aloha_lib.LorentzObject.__init__(self, order_lor, \
                                                  [], prefactor=sign * prefactor)
 
 
     def create_representation(self):
 
-        if not self.epsilon:
+        if not hasattr(self, 'epsilon'):
             # init all element to zero
             epsilon = dict( ((l1, l2, l3, l4), 0)
                                   for l1 in range(4) \
@@ -481,7 +493,7 @@ class Epsilon(aloha_lib.LorentzObject):
                                   for l4 in range(4))        
             # update non trivial one
             epsilon.update(dict(
-             ((l1, l2, l3, l4), give_sign_perm((l1,l2,l3,l4), [0,1,2,3]))
+             ((l1, l2, l3, l4), self.give_parity((l1,l2,l3,l4)))
                                  for l1 in range(4) \
                                  for l2 in range(4) if l2 != l1\
                                  for l3 in range(4) if l3 not in [l1,l2]\
