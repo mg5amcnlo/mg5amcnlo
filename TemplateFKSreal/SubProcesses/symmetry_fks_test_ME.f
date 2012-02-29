758a8af8a136fb893efac240a36c24ffc9431992
Marco Zaro <marco.zaro@gmail.com>
2012-02-29 17:12:15 +0100

    modified symmetry_fks_test_ME in TemplateFKS****/SubProcesses now the tests are correct

--- TemplateFKSreal/SubProcesses/symmetry_fks_test_ME.f
+++ TemplateFKSreal/SubProcesses/symmetry_fks_test_ME.f
@@ -37,7 +37,6 @@ c$$$      double precision pmass(-max_branch:-1,lmaxconfigs)   !Propagotor mass
       integer biforest(2,-max_branch:-1,lmaxconfigs)
       integer fksmother,fksgrandmother,fksaunt,i_fks,j_fks,compare
       integer fksconfiguration,mapbconf(0:lmaxconfigs)
-      integer r2b(lmaxconfigs),b2r(lmaxconfigs)
       logical searchforgranny,is_beta_cms,is_granny_sch,topdown,non_prop
       integer nbranch,ns_channel,nt_channel
       include "fks.inc"
@@ -106,6 +105,9 @@ c Particle types (=color) of i_fks, j_fks and fks_mother
       integer i_type,j_type,m_type
       common/cparticle_types/i_type,j_type,m_type
 
+      integer b_from_r(lmaxconfigs), r_from_b(lmaxconfigs)
+      include 'bornfromreal.inc'
+
 c
 c     External
 c
@@ -169,7 +171,6 @@ c
       nconfigs = 1
 c      write (*,*) mapconfig(0)
 
-      use_config(0)=0
 c Read FKS configuration from file
       open (unit=61,file='config.fks',status='old')
       read(61,'(I2)',err=99,end=99) fksconfiguration
@@ -179,67 +180,6 @@ c Use the fks.inc include file to set i_fks and j_fks
       j_fks=fks_j(fksconfiguration)
       write (*,*) 'FKS configuration number is ',fksconfiguration
       write (*,*) 'FKS partons are: i=',i_fks,'  j=',j_fks
-
-c Remove diagrams which do not have the correct structure
-c for the FKS partons i_fks and j_fks from the list of
-c integration channels.
-      write (*,*) 'Linking FKS configurations to Born configurations...'
-      open (unit=14,file='bornfromreal.dat',status='unknown')
-      searchforgranny=.false.
-      do i=1,mapconfig(0)
-         fksmother=0
-c     find number of s and t channels
-         if (j_fks.eq.2)then
-c     then t-channels in configs.inc are inverted.
-            compare=2
-         else
-c     t-channels are ordered normally 
-            compare=1
-         endif
-         nbranch=nexternal-2
-         ns_channel=1
-         do while(iforest(1,-ns_channel,i) .ne. compare .and.
-     &            ns_channel.lt.nbranch)
-            ns_channel=ns_channel+1         
-         enddo
-         ns_channel=ns_channel - 1
-         nt_channel=nbranch-ns_channel-1
-         call grandmother_fks(i,nbranch,ns_channel,
-     &        nt_channel,i_fks,j_fks,searchforgranny,
-     &        fksmother,fksgrandmother,fksaunt,
-     &        is_beta_cms,is_granny_sch,topdown)
-         non_prop=.false.
-c Skip diagrams with non-propagating particles:
-         do j=1,ns_channel
-            if (sprop(-j,i).eq.99) non_prop=.true.
-         enddo
-         do j=ns_channel+1,ns_channel+nt_channel
-            if (tprid(-j,i).eq.99) non_prop=.true.
-         enddo
-         if (fksmother.ne.0.and..not.non_prop)then !Found good diagram
-            use_config(0) = use_config(0)+1 ! # of good configs found so far
-c For each diagrams that contributes to a FKS configuration, there
-c must be a corresponding Born diagram. Link the diagrams in
-c configs.inc to the Born diagrams in born_conf.inc.
-c$$$            call link_to_born(iforest(1,-max_branch,i),i,i_fks,j_fks,
-c$$$     &                  fksmother, nbranch,mapbconf, r2b(i), biforest)
-            call link_to_born2(iforest(1,-max_branch,i),sprop(-max_branch,i),
-     &           tprid(-max_branch,i),i,i_fks,j_fks,fksmother,nbranch,
-     &           ns_channel,nt_channel,mapbconf, r2b(i), biforest)
-            write (14,'(a7,I4,a9,I4,a15,I4,a9,I4)') 'config ',i,
-     &           ' goes to ',r2b(i),', i.e. diagram ',mapconfig(i),
-     &           ' goes to ',mapbconf(r2b(i))
-            b2r(r2b(i))=i       ! also need inverse 
-            iconfig=mapconfig(i)
-         endif
-      enddo
-      close (14)
-      if (use_config(0).ne.mapbconf(0))then
-         write (*,*) 'FATAL ERROR 101 in symmetry',
-     &                 use_config(0),mapbconf(0)
-c         stop
-      endif
-      write (*,*) '...Configurations linked'
 
 
 c Set color types of i_fks, j_fks and fks_mother.
@@ -314,9 +254,9 @@ c$$$      write(*,*)'Using iconfig=',iconfig
          ntry=1
 
          if (iconfig_in.le.0) then
-            iconfig=mapconfig(b2r(bs))
-            minconfig=mapconfig(b2r(bs))
-            maxconfig=mapconfig(b2r(bs))
+            iconfig=mapconfig(r_from_b(mapbconf(bs)))
+            minconfig=mapconfig(r_from_b(mapbconf(bs)))
+            maxconfig=mapconfig(r_from_b(mapbconf(bs)))
          else
             iconfig=bs
             minconfig=iconfig
