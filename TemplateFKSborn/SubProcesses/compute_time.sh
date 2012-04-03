76909b90b9f0727de49f165cfcf7eeeab0c3dd85
Rikkert Frederix <frederix@physik.uzh.ch>
2012-04-03 09:40:17 +0200

    Added a shell script that computes the time from the info in the log files.

---
+++ TemplateFKSborn/SubProcesses/compute_time.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+
+time=0
+for pdir in P0*_* ; do
+    for dir in $pdir/$@ ; do
+	x1=`grep 'Time in seconds' $dir/log.txt`
+	time=`expr $time + ${x1##* }`
+    done
+done
+
+printf "total time: %02d days %02d hours %02d minutes %02d seconds\n" "$((time/86400))" "$((time/3600%24))" "$((time/60%60))" "$((time%60))"
