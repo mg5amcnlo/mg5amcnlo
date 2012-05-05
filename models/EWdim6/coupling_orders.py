537456549eb3b1b46a225a23821fce4a7e324278
Olivier Mattelaer <olivier.mattelaer@uclouvain.be>
2012-05-05 11:56:46 -0500

    adding model EWdim6 done by C. Degrande and asked to be available on the web by T. Stelzer

---
+++ models/EWdim6/coupling_orders.py
@@ -0,0 +1,20 @@
+# This file was automatically created by FeynRules $Revision: 1167 $
+# Mathematica version: 8.0 for Linux x86 (64-bit) (February 23, 2011)
+# Date: Fri 4 May 2012 16:30:01
+
+
+from object_library import all_orders, CouplingOrder
+
+
+NP = CouplingOrder(name = 'NP',
+                   expansion_order = 2,
+                   hierarchy = 1)
+
+QCD = CouplingOrder(name = 'QCD',
+                    expansion_order = 99,
+                    hierarchy = 1)
+
+QED = CouplingOrder(name = 'QED',
+                    expansion_order = 99,
+                    hierarchy = 2)
+
