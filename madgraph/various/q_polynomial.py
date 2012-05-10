8592461e5155f3a7a075b98578ef1686d4ee85b9
Valentin Hirschi <Spooner@tsf-wpa-3-1032.epfl.ch>
2012-05-10 13:01:15 +0200

    1. Q-polynomial will most likely not be important to make it a module.

--- madgraph/various/q_polynomial.py
+++ madgraph/various/q_polynomial.py
@@ -3,6 +3,11 @@ import copy
 
 class PolynomialError(Exception): pass
 
+def get_number_of_coefs_for_rank(r):
+    """ Returns the number of independent coefficients there is in a
+    fully symmetric tensor of rank r """
+    return sum([((3+ri)*(2+ri)*(1+ri))/6 for ri in range(0,r+1)])
+
 class polynomial(object):
     """ A class to represent a polynomial in the loop momentum (4-vector) q"""
     
