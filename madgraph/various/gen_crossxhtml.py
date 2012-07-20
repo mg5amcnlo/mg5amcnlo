07e646f9eda9a33e6f6cf22d0f919bb54b8b69fd
olivier Mattelaer <olivier.mattelaer@uclouvain.be>
2012-07-20 12:13:37 +0200

    Ooops....

--- madgraph/various/gen_crossxhtml.py
+++ madgraph/various/gen_crossxhtml.py
@@ -384,7 +384,7 @@ class RunResults(list):
         """initialize the object"""
         
         self.info = {'run_name': run_name,'me_dir':path}
-        self.tags = [run_card['run_name']]
+        self.tags = [run_card['run_tag']]
         # Set the collider information
         data = process.split('>',1)[0].split()
         if len(data) == 2:
