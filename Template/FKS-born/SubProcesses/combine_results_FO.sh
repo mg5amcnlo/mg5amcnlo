de2924208d5419b3505116c973951379c6a2ac97
Rikkert Frederix <frederix@physik.uzh.ch>
2012-08-21 14:53:31 +0200

    compute_time.sh and combine_results_FO.sh can take an argument to look at mint modes 0, 1 or 2.

--- Template/FKS-born/SubProcesses/combine_results_FO.sh
+++ Template/FKS-born/SubProcesses/combine_results_FO.sh
@@ -8,6 +8,18 @@ if [[ -d ./SubProcesses ]]; then
     cd SubProcesses
 fi
 
+if [[ $1 == "0" ]] ; then
+    mint_mode=_MINT0
+    shift
+elif [[ $1 == "1" ]] ; then
+    mint_mode=_MINT1
+    shift
+elif [[ $1 == "2" ]] ; then
+    echo "Cannot combine results for mint_mode 2"
+    exit
+else
+    mint_mode=
+fi
 
 if [[ -e res.txt ]]; then
     rm -f res.txt
@@ -23,7 +35,7 @@ for dir in "$@" ; do
     N=`ls -d P*/$dir | wc -l`
     NTOT=`expr $NTOT + $N`
     ls -d P0*/$dir >> dirs.txt
-    grep -H 'Final result:' P0*/$dir/log.txt >> res.txt
+    grep -H 'Final result:' P0*/$dir/log$mint_mode.txt >> res.txt
 done
 
 sed -i.bak s/"\+\/\-"/" \+\/\-"/ res.txt
