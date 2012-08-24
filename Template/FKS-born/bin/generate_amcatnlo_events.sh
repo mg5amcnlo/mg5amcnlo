b61e54d528023245a16b0bcd2c1bb8bbc7498921
Rikkert Frederix <frederix@physik.uzh.ch>
2012-08-24 15:14:23 +0200

    Improvement in generate_amcatnlo_events.sh to allow for separate F and V-events. Also updated sumres.py to have all events assigned randomly.

--- Template/FKS-born/bin/generate_amcatnlo_events.sh
+++ Template/FKS-born/bin/generate_amcatnlo_events.sh
@@ -15,36 +15,69 @@ fi
 
 Maindir=`pwd`
 
-if [[ "$@" == "" ]]; then
-    echo "Please give event types that should be calculated"
-    echo "e.g. 'F V', as arguments of this script" 
-    exit
+if [[ "$1" == "" ]]; then
+    echo "Please, enter the run mode"
+    echo " 1 : Born only"
+    echo " 2 : NLO (combined tree-level and virtual contributions)"
+    echo " 3 : NLO (separate channels for tree-level and virtual contributions)"
+    read run_mode_num
+else
+    run_mode_num=$1
 fi
 
-for run_mode in "$@" ; do
-    echo 'Cleaning previous results in the SubProcesses/P*/G'$run_mode'* directories'
-    rm -rf SubProcesses/P*/G$run_mode*/
-done
-
+if [[ $run_mode_num == 1 ]]; then
+    echo 'Cleaning previous results in the SubProcesses/P*/GB* directories'
+    rm -rf SubProcesses/P*/GB*/
+    echo "Setting inputs to run for 'born' in madinMMC_B.2"
+    sed -i.bak "11s/.*/born /" $Maindir/SubProcesses/madinMMC_B.2
+elif [[ $run_mode_num == 2 ]]; then
+    echo 'Cleaning previous results in the SubProcesses/P*/GF* directories'
+    rm -rf SubProcesses/P*/GF*/
+    echo "Setting inputs to run 'all' in madinMMC_F.2"
+    sed -i.bak "11s/.*/all  /" $Maindir/SubProcesses/madinMMC_F.2
+elif [[ $run_mode_num == 3 ]]; then
+    echo 'Cleaning previous results in the SubProcesses/P*/GF* directories'
+    rm -rf SubProcesses/P*/GF*/
+    echo "Setting inputs to run 'novB' in madinMMC_F.2"
+    sed -i.bak "11s/.*/novB /" $Maindir/SubProcesses/madinMMC_F.2
+    echo 'Cleaning previous results in the SubProcesses/P*/GV* directories'
+    rm -rf SubProcesses/P*/GV*/
+    echo "Setting inputs to run 'viSB' in madinMMC_V.2"
+    sed -i.bak "11s/.*/viSB /" $Maindir/SubProcesses/madinMMC_V.2
+else
+    echo "Input not understood:" $run_mode_num
+    exit
+fi
+    
 nevents=`awk '/^[^#].*=.*nevents/{print $1}' $Maindir/Cards/run_card.dat`
 parton_shower=`awk '/^[^#].*=.*parton_shower/{print $1}' $Maindir/Cards/run_card.dat`
 echo "Generating" $nevents "events for" $parton_shower
 
-
-
-for run_mode in "$@" ; do
-    $Maindir/bin/run_amcatnlo.sh $run_mode none 0 0 $parton_shower
-done
-$Maindir/SubProcesses/combine_results.sh 0 $nevents G$@\*
-
-for run_mode in "$@" ; do
-    $Maindir/bin/run_amcatnlo.sh $run_mode none 0 1 $parton_shower
-done
-$Maindir/SubProcesses/combine_results.sh 1 $nevents G$@\*
-
-for run_mode in "$@" ; do
-    $Maindir/bin/run_amcatnlo.sh $run_mode none 0 2 $parton_shower
-done
+if [[ $run_mode_num == 1 ]]; then
+    $Maindir/bin/run_amcatnlo.sh B none 0 0 $parton_shower
+    $Maindir/SubProcesses/combine_results.sh 0 $nevents GB*
+    $Maindir/bin/run_amcatnlo.sh B none 0 1 $parton_shower
+    $Maindir/SubProcesses/combine_results.sh 1 $nevents GB*
+    $Maindir/bin/run_amcatnlo.sh B none 0 2 $parton_shower
+elif [[ $run_mode_num == 2 ]]; then
+    $Maindir/bin/run_amcatnlo.sh F none 0 0 $parton_shower
+    $Maindir/SubProcesses/combine_results.sh 0 $nevents GF*
+    $Maindir/bin/run_amcatnlo.sh F none 0 1 $parton_shower
+    $Maindir/SubProcesses/combine_results.sh 1 $nevents GF*
+    $Maindir/bin/run_amcatnlo.sh F none 0 2 $parton_shower
+elif [[ $run_mode_num == 3 ]]; then
+    $Maindir/bin/run_amcatnlo.sh F none 0 0 $parton_shower
+    $Maindir/bin/run_amcatnlo.sh V none 0 0 $parton_shower
+    $Maindir/SubProcesses/combine_results.sh 0 $nevents GF* GV*
+    $Maindir/bin/run_amcatnlo.sh F none 0 1 $parton_shower
+    $Maindir/bin/run_amcatnlo.sh V none 0 1 $parton_shower
+    $Maindir/SubProcesses/combine_results.sh 1 $nevents GF* GV*
+    $Maindir/bin/run_amcatnlo.sh F none 0 2 $parton_shower
+    $Maindir/bin/run_amcatnlo.sh V none 0 2 $parton_shower
+else
+    echo "Input not understood" $run_mode_num
+    exit
+fi
 
 reweight_scale=`awk '/^[^#].*=.*reweight_scale/{print $1}' $Maindir/Cards/run_card.dat`
 reweight_PDF=`awk '/^[^#].*=.*reweight_PDF/{print $1}' $Maindir/Cards/run_card.dat`
@@ -81,7 +114,6 @@ if [[ $muF1_over_ref == $muF2_over_ref ]] ; then
 else
     echo "Cannot include reweight information: muF1_over_ref != muF2_over_ref"
 fi
-
 
 ./SubProcesses/reweight_xsec_events.sh < rwgt.temp
 
