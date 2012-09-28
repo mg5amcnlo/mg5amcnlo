75d9626c8071a1a374d803bdb14f95eb076e512e
olivier Mattelaer <olivier.mattelaer@uclouvain.be>
2012-09-28 03:07:26 -0500

    put path of auto-update to the final destination

--- madgraph/interface/madgraph_interface.py
+++ madgraph/interface/madgraph_interface.py
@@ -3208,8 +3208,7 @@ class MadGraphCmd(HelpToCmd, CheckValidForCmd, CompleteForCmd, CmdExtended):
         signal.alarm(timeout)
         to_update = 0
         try:
-#            filetext = urllib.urlopen('http://madgraph.phys.ucl.ac.be/mg5_build_nb')
-            filetext = urllib.urlopen('http://madgraph.phys.ucl.ac.be/mg5_test_build_nb')
+            filetext = urllib.urlopen('http://madgraph.phys.ucl.ac.be/mg5_build_nb')
             signal.alarm(0)
             web_version = int(filetext.read().strip())            
         except (TimeOutError, ValueError, IOError):
@@ -3252,8 +3251,8 @@ class MadGraphCmd(HelpToCmd, CheckValidForCmd, CompleteForCmd, CmdExtended):
             fail = 0
             for i in range(data['version_nb'], web_version):
                 try:
-#                    filetext = urllib.urlopen('http://madgraph.phys.ucl.ac.be/patch/build%s.patch' %(i+1))
-                    filetext = urllib.urlopen('http://madgraph.phys.ucl.ac.be/patch_test/build%s.patch' %(i+1))
+                    filetext = urllib.urlopen('http://madgraph.phys.ucl.ac.be/patch/build%s.patch' %(i+1))
+#                    filetext = urllib.urlopen('http://madgraph.phys.ucl.ac.be/patch_test/build%s.patch' %(i+1))
                 except:
                     print 'fail to load patch to build #%s' % (i+1)
                     fail = i
