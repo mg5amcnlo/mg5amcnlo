cb782a4a77e7aea01c2ff8c14f4dcb713e846a7e
Valentin Hirschi <Spooner@ValentinComputer.local>
2012-10-01 02:54:53 -0300

    1. Added a basic MadLoop tutorial you can get with 'tutorial MadLoop'.    It will be improved with a part on the specific 'check' command for    loop processes and also the launch command for MadLoop SA will give    more user-friendly output.
    2. Fixed a detail about last fix of rw_para.f

--- madgraph/interface/tutorial_text_madloop.py
+++ madgraph/interface/tutorial_text_madloop.py
@@ -21,9 +21,9 @@ this one.
 
 Remember that exactly as in MadGraph5, you can learn more about the different 
 options for any command by typing
-aMC@NLO>help A_CMD
+aMC@NLO> help A_CMD
 And to see a list of all commands, use
-aMC@NLO>help 
+aMC@NLO> help 
 
 MadLoop is the part of MadGraph5 used by aMC@NLO to generate the code for
 evaluating the loop diagrams. This tutorial teaches you how to use MadLoop
@@ -40,7 +40,7 @@ Let's start with the first point, how to generate a process with MadLoop in
 standalone mode. Keep in mind that this means only the loop and born diagrams
 are generated.
 
-mg5>generate g g > t t~ [virt=QCD]
+mg5> generate g g > d d~ [virt=QCD]
 
 Note that a space is mandatory between the particle names and that '[virt=QCD]' 
 specifies that you want to consider QCD NLO corrections. The keyword option
@@ -53,68 +53,75 @@ the header of your interface will change from 'mg5>' to 'ML5>'.
 generate = """
 You have just generated a new process.
 You can find more information on supported syntax by using:
-ML5>help generate
+ML5> help generate
 To list all defined processes, type
 ML5>display processes
 
 You can display a pictorial representation of the diagrams with 
-ML5>display diagrams
+ML5> display diagrams
 Notice you can add the option 'loop' or 'born' if you only want those diagrams
 to be displayed.
 
 If you want to add a second process, you can use the add process command:
-ML5> add process u u~ > t t~ [virt=QCD]
+ML5> add process e+ e- > d d~ [virt=QCD]
 But keep in mind that you must still consider only virtual corrections and 
 cannot employ multiparticle labels. Also decay chains are not available for
 loops.
 
 At this stage you can export your processes.
 This is done simply by typing:
-ML5>output MY_FIRST_MADLOOP_RUN
+ML5> output MY_FIRST_MADLOOP_RUN
+Notice that the standalone output mode (implicit in the above) is the only
+available for MadLoop standalone runs.
 """
 
 display_processes = """
 You have seen a list of the already defined processes.
 
-At this stage you can export your processes to different formats. In
-this tutorial, we will explain how to create a valid output for
-MadEvent. This is done simply by typing:
-aMC@NLO>output MY_FIRST_AMCTANLO_RUN
+At this stage you can export your processes to different formats. 
+To create a MadLoop standalone output for these, simply type:
+ML5> output MY_FIRST_MADLOOP_RUN
+"""
+
+display_diagrams = """
+You have displayed the diagrams.
+Notice you can add the 'born' or 'loop' option to this command to specify the
+class of diagrams to be displayed.
 """
 
 add_process = """
 You have added a process to your process list.
 
 At this stage you can export your processes.
-This is done simply by typing:
-aMC@NLO>output MY_FIRST_AMCATNLO_RUN
+For this, simply type
+ML5> output MY_FIRST_MADLOOP_RUN
 """
+
 output = """
-If you are following the tutorial, a directory MY_FIRST_MG5_RUN has
-been created which can be used in order to run aMC@NLO.
+If you are following the tutorial, a directory MY_FIRST_MADLOOP_RUN has
+been created under your MadGraph5 installation directory.
 
-Additionally to the commands in the bin directory (see 
-MY_FIRST_MG5_RUN/README), you can also generate your events/compute the 
-cross-section from this interface. 
-You will generate events to be showered a la MC@NLO, compute the theoretical
-and PDF error on the fly (if asked for in the run_card.dat) and shower the events 
-with the parton_shower MonteCarlo specified in the run_card.dat, generating a file in
-the StdHEP format.
-Please Enter:
-aMC@NLO> launch
-(you can interrupt the computation to continue the tutorial by pressing Ctrl-C)
+The code for the evaluation of the squared loop matrix element is in 
+'SubProcesses/P0_<shell_proc_name>/'. There, you can compile and edit 
+running parameters from 'MadloopParams.dat' and then run the code with './check'.
+Alternatively, for a simple quick run, type:
+ML5> launch
+This computes the squared matrix element for three successive PS points and
+outputs the result for the third one. For the purpose of this tutorial, you 
+can skip the edition of the param_card and keep the default values.
 """
 
-open_index = output
+launch = """
+You just launched the MadLoop standalone evalutation of the squared loop matrix
+element for (a/many) specific process(es) for a random Phase-Space point.
+The two processes proposed in this tutorial where g g > d d~ and e+ e- > d d~.
+You can check that you get the right double pole normalized with respect to
+the born*(alpha_s/2*pi), namely -26/3 and -8/3 respectively.
 
-launch = """This step ends the tutorial of the basic commands of aMCatNLO. You can
-always use the help to see the options available for different
-commands. For example, if you want to know how to launch on multicore/cluster
-just type
-aMC@NLO>help launch
-
-In order to close this tutorial, enter
-aMC@NLO>tutorial stop
-If you want to exit MG5, enter
-aMC@NLO>exit
+This tutorial will soon be complemented with a part describing the various 
+additional 'check' commands solely for MadLoop standalone.
+But for now, you can close this tutorial by typing
+ML5>tutorial stop
+And if you want to exit MG5, enter
+ML5>exit
 """
