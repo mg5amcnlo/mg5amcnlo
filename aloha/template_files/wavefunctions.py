1f67a5e6795ea249834b85cd5119665309954c61
olivier Mattelaer <olivier.mattelaer@uclouvain.be>
2013-01-29 15:08:15 -0600

    fix some bug in irxxx and modify the spin32 propagator

--- aloha/template_files/wavefunctions.py
+++ aloha/template_files/wavefunctions.py
@@ -388,20 +388,20 @@ def irxxxx(p, mass, nhel, nsr):
             #ep[0] = 0
             ep[3] = pt/pp*sqh
             if pt:
-               pzpt = p[3]/(pp*pt)*sqh
-               ep[1] = complex( -p[1]*pzpt , -nsv*p[2]/pt*sqh )
-               ep[2] = complex( -p[2]*pzpt ,  nsv*p[1]/pt*sqh )
+                pzpt = p[3]/(pp*pt)*sqh
+                ep[1] = complex( -p[1]*pzpt , -nsv*p[2]/pt*sqh )
+                ep[2] = complex( -p[2]*pzpt ,  nsv*p[1]/pt*sqh )
             else:
-               ep[1] = -sqh 
-               ep[2] = complex( 0 , nsv*sign(sqh,p[3]) )
+                ep[1] = -sqh 
+                ep[2] = complex( 0 , nsv*sign(sqh,p[3]) )
         else:
             #ep[0] = 0d0
             ep[1] = -sqh
-            ep[2] = complex(0, nsv * sign(sqh,p(3)))
+            ep[2] = complex(0, nsv * sqh)
             #ep[3] = 0d0
 
          
-    if ( nhel <= 0 ): 
+    if ( nhel < 0 ): 
         #construct eps-
         em = [0] * 4
         if ( pp == 0 ):
@@ -413,12 +413,12 @@ def irxxxx(p, mass, nhel, nsr):
             #em[0] = 0
             em[3] = -pt/pp*sqh
             if pt:
-               pzpt = -p[3]/(pp*pt)*sqh
-               em[1] = complex( -p[1]*pzpt , -nsv*p[2]/pt*sqh )
-               em[2] = complex( -p[2]*pzpt ,  nsv*p[1]/pt*sqh )
+                pzpt = -p[3]/(pp*pt)*sqh
+                em[1] = complex( -p[1]*pzpt , -nsv*p[2]/pt*sqh )
+                em[2] = complex( -p[2]*pzpt ,  nsv*p[1]/pt*sqh )
             else:
-               em[1] = sqh
-               em[2] = complex( 0 , nsv*sign(sqh,p[3]) )            
+                em[1] = sqh
+                em[2] = complex( 0 , nsv*sign(sqh,p[3]) )            
                 
     if ( abs(nhel) <= 1 ):  
         #construct eps0
@@ -461,8 +461,8 @@ def irxxxx(p, mass, nhel, nsr):
                 sf[1] = float(1+nsr-(1-nsr)*nh)*0.5
                 omega[0] = sqrt(p[0]+pp)
                 omega[1] = mass/omega[0]
-                ip = (1+nh)//2 
-                im = (1-nh)//2 
+                ip = ((3+nh)//2) -1 # -1 since they are index 
+                im = ((3-nh)//2) -1 # -1 since they are index
                 sfomeg[0] = sf[0]*omega[ip]
                 sfomeg[1] = sf[1]*omega[im]
                 pp3 = max([pp+p[3],0])
@@ -622,7 +622,6 @@ def orxxxx(p, mass, nhel, nsr):
     pt2 = p[1]**2 + p[2]**2
     pp = min([p[0], sqrt(pt2+p[3]**2)])
     pt = min([pp,sqrt(pt2)])
-    
     rc = {}
     rc[(4,0)] = complex(p[0],p[3])*nsr
     rc[(5,0)] = complex(p[1],p[2])*nsr
@@ -637,12 +636,12 @@ def orxxxx(p, mass, nhel, nsr):
             #ep[0] = 0
             ep[3] = pt/pp*sqh
             if pt:
-               pzpt = p[3]/(pp*pt)*sqh
-               ep[1] = complex( -p[1]*pzpt , -nsv*p[2]/pt*sqh )
-               ep[2] = complex( -p[2]*pzpt ,  nsv*p[1]/pt*sqh )
+                pzpt = p[3]/(pp*pt)*sqh
+                ep[1] = complex( -p[1]*pzpt , -nsv*p[2]/pt*sqh )
+                ep[2] = complex( -p[2]*pzpt ,  nsv*p[1]/pt*sqh )
             else:
-               ep[1] = -sqh 
-               ep[2] = complex( 0 , nsv*sign(sqh,p[3]) )
+                ep[1] = -sqh 
+                ep[2] = complex( 0 , nsv*sign(sqh,p[3]) )
         else:
             #ep[0] = 0d0
             ep[1] = -sqh
@@ -661,12 +660,12 @@ def orxxxx(p, mass, nhel, nsr):
             #em[0] = 0
             em[3] = -pt/pp*sqh
             if pt:
-               pzpt = -p[3]/(pp*pt)*sqh
-               em[1] = complex( -p[1]*pzpt , -nsv*p[2]/pt*sqh )
-               em[2] = complex( -p[2]*pzpt ,  nsv*p[1]/pt*sqh )
+                pzpt = -p[3]/(pp*pt)*sqh
+                em[1] = complex( -p[1]*pzpt , -nsv*p[2]/pt*sqh )
+                em[2] = complex( -p[2]*pzpt ,  nsv*p[1]/pt*sqh )
             else:
-               em[1] = sqh
-               em[2] = complex( 0 , nsv*sign(sqh,p[3]) )            
+                em[1] = sqh
+                em[2] = complex( 0 , nsv*sign(sqh,p[3]) )            
                 
     if ( abs(nhel) <= 1 ):  
         #construct eps0
@@ -709,8 +708,8 @@ def orxxxx(p, mass, nhel, nsr):
                 sf[1] = (1+nsr-(1-nsr)*nh)*0.5
                 omega[0] = sqrt(p[0]+pp)
                 omega[1] = mass/omega[0]
-                ip = (1+nh)//2 
-                im = (1-nh)//2 
+                ip = (3+nh)//2  -1 # -1 since this is index
+                im = (3-nh)//2  -1 # -1 since this is index 
                 sfomeg[0] = sf[0]*omega[ip]
                 sfomeg[1] = sf[1]*omega[im]
                 pp3 = max([pp+p[3],0])
@@ -765,10 +764,10 @@ def orxxxx(p, mass, nhel, nsr):
                 ip = -((1+nh)/2)
                 im =  (1-nh)/2
             
-                fom[1] = im     * sqm[im]
-                fom[2] = ip*nsr * sqm[im]
-                fom[3] = im*nsr * sqm[-ip]
-                fom[4] = ip     * sqm[-ip]
+                fom[0] = im     * sqm[im]
+                fom[1] = ip*nsr * sqm[im]
+                fom[2] = im*nsr * sqm[-ip]
+                fom[3] = ip     * sqm[-ip]
             
             else:
                 pp = min([p[0],sqrt(p[1]**2+p[2]**2+p[3]**2)])
@@ -776,8 +775,8 @@ def orxxxx(p, mass, nhel, nsr):
                 sf[1] = (1+nsr-(1-nsr)*nh)*0.5
                 omega[0] = sqrt(p[0]+pp)
                 omega[1] = mass/omega[0]
-                ip = (1+nh)//2
-                im = (1-nh)//2
+                ip = (3+nh)//2 -1 #-1 since ip is an index
+                im = (3-nh)//2 -1 
                 sfomeg[0] = sf[0]*omega[ip]
                 sfomeg[1] = sf[1]*omega[im]
                 pp3 = max([pp+p[3], 0])
@@ -810,8 +809,8 @@ def orxxxx(p, mass, nhel, nsr):
             else:
                 #fom[1] = 0
                 #fom[2] = 0
-                fom[3] = chi[1]
-                fom[4] = chi[0]
+                fom[2] = chi[1]
+                fom[3] = chi[0]
 
     cond1 = ( pt==0 and p[3]>=0)
     cond2= (pt==0 and p[3]<0)
