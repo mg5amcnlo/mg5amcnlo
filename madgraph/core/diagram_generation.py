a4bb287aaa253f9c3aa975c30a2bd8bbcb111189
Johan Alwall <johan@alwall.net>
2013-02-25 08:52:53 +0800

    Added run_delphes3 script for running Delphes 3 on an STDHEP file

--- madgraph/core/diagram_generation.py
+++ madgraph/core/diagram_generation.py
@@ -1045,7 +1045,7 @@ class DecayChainAmplitude(Amplitude):
                  "$RED Decay without corresponding particle in core process found.\n" + \
                  "Decay information for particle(s) %s is discarded.\n" % ','.join(names) + \
                  "Please check your process definition carefully. \n" + \
-                 "This error usually means that you FORGET parenthesis in presence of subdecay.\n" + \
+                 "This error usually means that you forgot parentheses in presence of subdecay.\n" + \
                  "Example of correct syntax: p p > t t~, ( t > w+ b, w+ > l+ vl)")
 
                 # Remove unused decays from the process list
