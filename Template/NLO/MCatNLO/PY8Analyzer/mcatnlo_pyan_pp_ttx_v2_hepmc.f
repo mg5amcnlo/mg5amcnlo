4b1274eb07a39dd39a5664f818cda45de9a4fffc
Paolo Torrielli <torriell@physik.uzh.ch>
2014-03-07 08:46:55 +0100

    Updated the shower analyses so that no two histograms have the same name (which would cause the uncertainties script to get stuck)

--- Template/NLO/MCatNLO/PY8Analyzer/mcatnlo_pyan_pp_ttx_v2_hepmc.f
+++ Template/NLO/MCatNLO/PY8Analyzer/mcatnlo_pyan_pp_ttx_v2_hepmc.f
@@ -18,7 +18,7 @@ C----------------------------------------------------------------------
       PARAMETER (PI=3.14159265358979312D0)
       integer j,kk,l,i,nnn
       character*5 cc(2)
-      data cc/'     ','     '/
+      data cc/'     ','Born '/
       integer nwgt,max_weight,nwgt_analysis
       common/cnwgt/nwgt
       common/c_analysis/nwgt_analysis
