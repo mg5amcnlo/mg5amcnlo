25a4e1a5600cac5c215196215b1e700fc20c4690
olivier Mattelaer <olivier.mattelaer@uclouvain.be>
2014-03-12 17:51:14 +0000

    fix a small glitch in Pythia8 output

--- madgraph/interface/madgraph_interface.py
+++ madgraph/interface/madgraph_interface.py
@@ -5345,7 +5345,17 @@ class MadGraphCmd(HelpToCmd, CheckValidForCmd, CompleteForCmd, CmdExtended):
         options = config[self._export_format]
         # check
         if os.path.realpath(self._export_dir) == os.getcwd():
-            if not args[0] in ['.', '-f']:
+            if len(args) == 0:
+                i=0
+                while 1:
+                    if os.path.exists('Pythia8_proc_%i' %i):
+                        i+=1
+                    else:
+                        break
+                os.mkdir('Pythia8_proc_%i' %i) 
+                self._export_dir = pjoin(self._export_dir, 'Pythia8_proc_%i' %i)
+                logger.info('Create output in %s' % self._export_dir)
+            elif not args[0] in ['.', '-f']:
                 raise self.InvalidCmd, 'Wrong path directory to create in local directory use \'.\''
         elif not noclean and os.path.isdir(self._export_dir) and options['check']:
             if not force:
