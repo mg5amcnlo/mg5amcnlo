30e6e95bff4bbdb4869d7024b3f8c8de86f2f467
Marco Zaro <marco.zaro@gmail.com>
2014-03-13 15:06:30 +0100

    fixed bug for e+e- processes + lhapdf

--- madgraph/interface/amcatnlo_run_interface.py
+++ madgraph/interface/amcatnlo_run_interface.py
@@ -3125,11 +3125,15 @@ Integrated cross-section
             os.remove(pjoin(libdir, 'PDFsets'))
 
         # read the run_card to find if lhapdf is used or not
-        if self.run_card['pdlabel'] == 'lhapdf':
+        if self.run_card['pdlabel'] == 'lhapdf' and \
+                (self.banner.get_detail('run_card', 'lpp1') != '0' or \
+                 self.banner.get_detail('run_card', 'lpp1') != '0'):
             self.link_lhapdf(libdir)
         else:
-            if self.run_card['lpp1'] == '1' ==self.run_card['lpp2']:
+            if self.run_card['lpp1'] == '1' == self.run_card['lpp2']:
                 logger.info('Using built-in libraries for PDFs')
+            if self.run_card['lpp1'] == '0' == self.run_card['lpp2']:
+                logger.info('Lepton-Lepton collision: Ignoring \'pdlabel\' and \'lhaid\' in the run_card.')
             try:
                 del os.environ['lhapdf']
             except KeyError:
