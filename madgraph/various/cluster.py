79ed3468ddfaf787af9175019ce7c3225fa5256f
olivier Mattelaer <olivier.mattelaer@uclouvain.be>
2014-03-19 10:42:56 +0000

    fix a problem with PBS cluster (one more actually)

--- madgraph/various/cluster.py
+++ madgraph/various/cluster.py
@@ -967,6 +967,7 @@ class PBSCluster(Cluster):
 
         if len(self.submitted_ids) > self.maximum_submited_jobs:
             fct = lambda idle, run, finish: logger.info('Waiting for free slot: %s %s %s' % (idle, run, finish))
+            me_dir = os.path.realpath(os.path.join(cwd,prog)).rsplit('/SubProcesses',1)[0]
             self.wait(me_dir, fct, self.maximum_submited_jobs)
 
         
