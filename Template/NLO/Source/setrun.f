c344c3bdb134d3df8283bba71711d5f7b6debe19
Rikkert Frederix <frederix@physik.uzh.ch>
2014-05-16 10:09:51 +0200

    The internal NNPDF is the new default set for NLO running

--- Template/NLO/Source/setrun.f
+++ Template/NLO/Source/setrun.f
@@ -310,7 +310,7 @@ C-------------------------------------------------
       integer mpdf
       integer npdfs,i,pdfgup(2),pdfsup(2),lhaid
 
-      parameter (npdfs=13)
+      parameter (npdfs=16)
       character*7 pdflabs(npdfs)
       data pdflabs/
      $   'none',
@@ -325,7 +325,10 @@ C-------------------------------------------------
      $   'cteq5m1',
      $   'cteq6_m',
      $   'cteq6_l',
-     $   'cteq6l1'/
+     $   'cteq6l1',     
+     $   'nn23lo',
+     $   'nn23lo1',
+     $   'nn23nlo'/
       integer numspdf(npdfs)
       data numspdf/
      $   00000,
@@ -340,7 +343,10 @@ C-------------------------------------------------
      $   19051,
      $   10000,
      $   10041,
-     $   10042/
+     $   10042,
+     $   200200,
+     $   200400,
+     $   244600/
 
 
       if(pdfin.eq."lhapdf") then
