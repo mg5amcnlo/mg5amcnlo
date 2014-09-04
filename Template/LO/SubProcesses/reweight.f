10a0c80b8fa4491ae4bbcf34dd9c5e83b5319e33
olivier Mattelaer <olivier.mattelaer@uclouvain.be>
2014-09-04 18:29:17 +0100

    fix problem for p p > j j a a j in heft

--- Template/LO/SubProcesses/reweight.f
+++ Template/LO/SubProcesses/reweight.f
@@ -720,17 +720,21 @@ c          Check QCD jet, take care so not a decay
 c          Remove non-gluon jets that lead up to non-jet vertices
            if(ipart(1,imocl(n)).gt.2)then ! ipart(1) set and not IS line
 c          The ishft gives the FS particle corresponding to imocl
-              if(ipdgcl(ishft(1,ipart(1,imocl(n))-1),igraphs(1),iproc).ne.21)
-     $              iqjets(ipart(1,imocl(n)))=0
+              if(ipdgcl(ishft(1,ipart(1,imocl(n))-1),igraphs(1),iproc).ne.21)then
+                 iqjets(ipart(1,imocl(n)))=0
               else if (ipdgcl(imocl(n),igraphs(1),iproc).eq.21)
-c                   special case for g > g h remove also the hardest gluon
-                    iqjets(ipart(1,imocl(n)))=0
+c                special case for g > g h remove also the hardest gluon
+                 iqjets(ipart(1,imocl(n)))=0
               endif
            endif
            if(ipart(2,imocl(n)).gt.2)then ! ipart(1) set and not IS line
 c             The ishft gives the FS particle corresponding to imocl
-              if(ipdgcl(ishft(1,ipart(2,imocl(n))-1),igraphs(1),iproc).ne.21)
-     $              iqjets(ipart(2,imocl(n)))=0
+              if(ipdgcl(ishft(1,ipart(2,imocl(n))-1),igraphs(1),iproc).ne.21.and.
+     $                                   ipdgcl(imocl(n),igraphs(1),iproc).ne.21) then
+c                 The second condition is to prevent the case of ggh where the gluon split in quark later.
+c                 The first quark is already remove so we shouldn't remove this one.      
+              iqjets(ipart(2,imocl(n)))=0
+              endif
            endif
 c          Set goodjet to false for mother
               goodjet(imocl(n))=.false.
