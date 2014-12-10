28e9f471dedcadb4863885a4526bd06d76d6c480
olivier Mattelaer <olivier.mattelaer@uclouvain.be>
2014-12-10 14:58:42 +0000

    try to fix some warning raise with qsub for the survey

--- Template/LO/SubProcesses/survey.sh
+++ Template/LO/SubProcesses/survey.sh
@@ -1,57 +1,57 @@
 #!/bin/bash
 if [[ -e MadLoop5_resources.tar && ! -e MadLoop5_resources ]]; then
-tar -xf MadLoop5_resources.tar
+tar -xf MadLoop5_resources.tar;
 fi
-k=run1_app.log
-script=ajob1                         
-offset=$1
-shift
-subdir=$offset
+k=run1_app.log;
+script=ajob1;                      
+offset=$1;
+shift;
+subdir=$offset;
 for i in $@ ; do
-     j=G$i
+     j=G$i;
      if [[ $offset == *.* ]];then
-	 subdir=${offset%.*}
-	 offset=${offset##*.}
-	 j=G${i}_${subdir}	     
+	 subdir=${offset%.*};
+	 offset=${offset##*.};
+	 j=G${i}_${subdir};     
      elif [[ $offset -gt 0 ]]; then
-	 j=G${i}_${subdir}
+	 j=G${i}_${subdir};
      fi
      if [[ ! -e $j ]]; then
-         mkdir $j
+         mkdir $j;
      fi
-     cd $j
+     cd $j;
      if [[ $offset -eq 0 ]]; then
-	 rm -f ftn25 ftn26 ftn99
-	 rm -f $k
+	 rm -f ftn25 ftn26 ftn99;
+	 rm -f $k;
      else
-	 echo   "$offset"  > moffset.dat
+	 echo   "$offset"  > moffset.dat;
      fi
      if [[ $offset -eq $subdir ]]; then
-	 rm -f ftn25 ftn26 ftn99
-	 rm -f $k
+	 rm -f ftn25 ftn26 ftn99;
+	 rm -f $k;
      else
         if [[ -e ../ftn25 ]]; then
-	    cp ../ftn25 .
+	    cp ../ftn25 .;
 	fi
      fi
      if [[ ! -e input_app.txt  ]]; then
-	 cat ../input_app.txt >& input_app.txt
+	 cat ../input_app.txt >& input_app.txt;
      fi
-     echo $i >> input_app.txt
+     echo $i >> input_app.txt;
 
      for((try=1;try<=10;try+=1)); 
      do
-     ../madevent >> $k <input_app.txt
+     ../madevent >> $k <input_app.txt;
      if [ -s $k ]
      then
-         break
+         break;
      else
-     sleep 1
+     sleep 1;
      fi
      done
 #     rm -f ftn25 ftn99
 #     rm -f ftn26
-     echo "" >> $k; echo "ls status:" >> $k; ls >> $k
-     cp $k log.txt
-     cd ../
-done
+     echo "" >> $k; echo "ls status:" >> $k; ls >> $k;
+     cp $k log.txt;
+     cd ../;
+done;
