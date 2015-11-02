1e3b0b06a43d32e731d58d95a32e7ea960c7e885
olivier Mattelaer <olivier.mattelaer@uclouvain.be>
2015-11-02 16:41:13 +0000

    update TopEffTh model

--- models/TopEffTh/write_param_card.py
+++ models/TopEffTh/write_param_card.py
@@ -1,5 +1,5 @@
 
-__date__ = "3 june 2010"
+__date__ = "02 Aug 2012"
 __author__ = 'olivier.mattelaer@uclouvain.be'
 
 from function_library import *
@@ -28,6 +28,7 @@ class ParamCardWriter(object):
         self.fsock.write(self.header)
         
         self.write_card(list_of_parameters)
+        self.fsock.close()
     
     def define_not_dep_param(self, list_of_parameters):
         """define self.dep_mass and self.dep_width in case that they are 
@@ -117,6 +118,7 @@ class ParamCardWriter(object):
     def write_dep_param_block(self, lhablock):
         import cmath
         from parameters import all_parameters
+        from particles import all_particles
         for parameter in all_parameters:
             exec("%s = %s" % (parameter.name, parameter.value))
         text = "##  Not dependent paramater.\n"
@@ -130,6 +132,7 @@ class ParamCardWriter(object):
         else:
             data = self.dep_width
             prefix = "DECAY "
+
         for part, param in data:
             if isinstance(param.value, str):
                 value = complex(eval(param.value)).real
@@ -138,8 +141,32 @@ class ParamCardWriter(object):
             
             text += """%s %s %f # %s : %s \n""" %(prefix, part.pdg_code, 
                         value, part.name, param.value)
+        # If more than a particles has the same mass/width we need to write it here
+        # as well
+        if lhablock == 'MASS':
+            arg = 'mass'
+            done = [part for (part, param) in self.dep_mass]
+        else:
+            arg = 'width'
+            done = [part for (part, param) in self.dep_width]
+        for particle in all_particles:
+            if particle.pdg_code <0:
+                continue
+            is_define = True
+            if particle not in done:
+                if getattr(particle, arg).lhacode[0] != particle.pdg_code:
+                    is_define = False                
+            if  not is_define:
+                value = float(particle.get(arg).value )
+                name =  particle.get(arg).name 
+                text += """%s %s %f # %s : %s \n""" %(prefix, particle.pdg_code, 
+                        value, particle.name, name)
+
+
+
+
         self.fsock.write(text)    
-    
+        
     sm_pdg = [1,2,3,4,5,6,11,12,13,13,14,15,16,21,22,23,24,25]
     data="""Block QNUMBERS %(pdg)d  # %(name)s 
         1 %(charge)d  # 3 times electric charge
@@ -162,12 +189,11 @@ class ParamCardWriter(object):
             text += self.data % {'pdg': part.pdg_code,
                                  'name': part.name,
                                  'charge': 3 * part.charge,
-                                 'spin': 2 * part.spin + 1,
+                                 'spin': part.spin,
                                  'color': part.color,
                                  'antipart': part.name != part.antiname and 1 or 0}
         
         self.fsock.write(text)
-        
         
             
             
