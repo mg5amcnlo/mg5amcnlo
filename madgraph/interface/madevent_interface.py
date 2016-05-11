b26443933a80e23bbb290c119a55952aab835cd9
Valentin Hirschi <valentin.hirschi@gmail.com>
2016-05-11 01:08:49 -0700

    1. Fixed test of the main.pdf output

--- madgraph/interface/madevent_interface.py
+++ madgraph/interface/madevent_interface.py
@@ -3397,7 +3397,7 @@ Beware that this can be dangerous for local multicore runs.""")
             misc.sprint('Now launching MA5 with commands:\n','\n'.join(MA5_commands))
             MA5_interpreter.load(MA5_commands)
             
-            if not os.path.isdir(pjoin(self.me_dir,'MA5_PARTON_ANALYSIS','PDF','main.pdf')):
+            if not os.path.isfile(pjoin(self.me_dir,'MA5_PARTON_ANALYSIS','PDF','main.pdf')):
                 raise MadGraph5Error, "MadAnalysis5 failed to produced "+\
                     "an output for the parton analysis in\n   %s"%\
                        pjoin(self.me_dir,'MA5_PARTON_ANALYSIS','PDF','main.pdf')
