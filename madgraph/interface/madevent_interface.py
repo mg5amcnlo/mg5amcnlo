b6af0cb77e0ad37beaeb71a2db36bb007179a037
olivier Mattelaer <olivier.mattelaer@uclouvain.be>
2016-08-12 11:59:53 +0100

    try to fix py6 in condor

--- madgraph/interface/madevent_interface.py
+++ madgraph/interface/madevent_interface.py
@@ -3221,15 +3221,18 @@ Beware that this can be dangerous for local multicore runs.""")
         #                argument= [pythia_src], stdout= pythia_log,
         #                stderr=subprocess.STDOUT,
         #                cwd=pjoin(self.me_dir,'Events'))
+        output_files = ['pythia_events.hep']
+        if self.run_card['use_syst']:
+            output_files.append('syst.dat')
+        if self.run_card['ickkw'] == 1: 
+            output_files.append(['beforeveto.tree', 'xsecs.tree', 'events.tree'])
         
+        os.environ['PDG_MASS_TBL'] = pjoin(pythia_src,'mass_width_2004.mc')
         self.cluster.launch_and_wait(pjoin(pythia_src, 'pythia'),
                                      input_files=[pjoin(self.me_dir, "Events", "unweighted_events.lhe"),
-                                                  pjoin(self.me_dir,'Cards','pythia_card.dat')],
-                                     output_files=['syst.dat', 
-                                                   'beforeveto.tree',
-                                                   'pythia_events.hep'
-                                                   'xsecs.tree',
-                                                   'events.tree'],
+                                                  pjoin(self.me_dir,'Cards','pythia_card.dat'),
+                                                  pjoin(pythia_src,'mass_width_2004.mc')],
+                                     output_files=output_files,
                                      stdout= pythia_log,
                                      stderr=subprocess.STDOUT,
                                      cwd=pjoin(self.me_dir,'Events'))
