cfbd69bdd00b54ef66b5930ca79ba876577dbdc9
Valentin Hirschi <valentin.hirschi@gmail.com>
2016-08-12 22:33:56 -0700

    1. Cleaned up the PY8_CKKW module.

--- Template/LO/Source/BIAS/PY8_CKKW/py8_mg5amc_bias_interface.cc
+++ Template/LO/Source/BIAS/PY8_CKKW/py8_mg5amc_bias_interface.cc
@@ -3,10 +3,6 @@
 #include <math.h>
 #include <vector>
 #include <sstream>
-#include <algorithm> 
-#include <functional> 
-#include <cctype>
-#include <locale>
 
 // Small values used to cut off momenta
 const double TINY      = 1e-15;
@@ -15,37 +11,6 @@ const double TINYMASS  = 1e-8;
 bool initialization_done = false;
 
 Pythia8::Pythia pythia;
-
-// trim from start (in place)
-static inline void ltrim(std::string &s) {
-    s.erase(s.begin(), std::find_if(s.begin(), s.end(),
-            std::not1(std::ptr_fun<int, int>(std::isspace))));
-}
-// trim from end (in place)
-static inline void rtrim(std::string &s) {
-    s.erase(std::find_if(s.rbegin(), s.rend(),
-            std::not1(std::ptr_fun<int, int>(std::isspace))).base(), s.end());
-}
-// trim from both ends (in place)
-static inline void trim(std::string &s) {
-    ltrim(s);
-    rtrim(s);
-}
-// trim from start (copying)
-static inline std::string ltrimmed(std::string s) {
-    ltrim(s);
-    return s;
-}
-// trim from end (copying)
-static inline std::string rtrimmed(std::string s) {
-    rtrim(s);
-    return s;
-}
-// trim from both ends (copying)
-static inline std::string trimmed(std::string s) {
-    trim(s);
-    return s;
-}
 
 extern "C" {
 //Fortran interface
