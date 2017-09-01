90f4fcadde66c48c45de945d8c4601abad81f093
olivier-mattelaer <>
2017-09-01 12:32:55 +0200

    improve re-weighting structure to allow more plugin flexibility

--- madgraph/interface/reweight_interface.py
+++ madgraph/interface/reweight_interface.py
@@ -906,6 +906,33 @@ class ReweightInterface(extended_cmd.Cmd):
     def do_compute_widths(self, line):
         return self.mother.do_compute_widths(line)
     
+    
+    dynamical_scale_warning=True
+    def change_kinematics(self, event):
+ 
+
+        if isinstance(self.run_card, banner.RunCardLO):
+            jac = event.change_ext_mass(self.new_param_card)
+            new_event = event
+        else:
+            jac =1
+            new_event = event
+
+        if jac != 1:
+            if self.output_type == 'default':
+                logger.critical('mass reweighting requires dedicated lhe output!. Please include "change output 2.0" in your reweight_card')
+                raise Exception
+            mode = self.run_card['dynamical_scale_choice']
+            if mode == -1:
+                if self.dynamical_scale_warning:
+                    logger.warning('dynamical_scale is set to -1. New sample will be with HT/2 dynamical scale for renormalisation scale')
+                mode = 3
+            new_event.scale = event.get_scale(mode)
+            new_event.aqcd = self.lhe_input.get_alphas(new_event.scale, lhapdf_config=self.mother.options['lhapdf'])
+         
+        return jac, new_event
+    
+    
     def calculate_weight(self, event):
         """space defines where to find the calculator (in multicore)"""
         
@@ -918,27 +945,15 @@ class ReweightInterface(extended_cmd.Cmd):
         orig_wgt = event.wgt
         # LO reweighting    
         w_orig = self.calculate_matrix_element(event, 0)
+        
         # reshuffle event for mass effect # external mass only
-        if isinstance(self.run_card, banner.RunCardLO):
-            jac = event.change_ext_mass(self.new_param_card)
-        else:
-            jac =1
-
-        if jac != 1:
-            if self.output_type == 'default':
-                logger.critical('mass reweighting requires dedicated lhe output!. Please include "change output 2.0" in your reweight_card')
-                raise Exception
-            mode = self.run_card['dynamical_scale_choice']
-            if mode == -1:
-                logger.warning('dynamical_scale is set to -1. New sample will be with HT/2 dynamical scale for renormalisation scale')
-                mode = 3
-            event.scale = event.get_scale(mode)
-            event.aqcd = self.lhe_input.get_alphas(event.scale, lhapdf_config=self.mother.options['lhapdf'])
- 
+        # carefull that new_event can sometimes be = to event 
+        # (i.e. change can be in place)
+        jac, new_event = self.change_kinematics(event)
         
         
         if event.wgt != 0: # impossible reshuffling
-            w_new =  self.calculate_matrix_element(event, 1)
+            w_new =  self.calculate_matrix_element(new_event, 1)
         else:
             w_new = 0
 
