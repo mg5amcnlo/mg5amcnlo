ac3cc8f75d9fdc68d78f05c313fec7079546eeec
olivier-mattelaer <>
2018-03-16 13:13:34 +0100

    fixing symmetry.f to have correctly the computation of sqrts in the choice of channel

--- madgraph/iolibs/template_files/madevent_symmetry.f
+++ madgraph/iolibs/template_files/madevent_symmetry.f
@@ -102,8 +102,10 @@ c     Set stot
          if (abs(lpp(2)) .eq. 1 .or. abs(lpp(2)) .eq. 2) m2 = 0.938d0
          if (abs(lpp(1)) .eq. 3) m1 = 0.000511d0
          if (abs(lpp(2)) .eq. 3) m2 = 0.000511d0
-         if(ebeam(1).lt.m1) ebeam(1) = m1
-         if(ebeam(2).lt.m2) ebeam(2) = m2
+         if (mass_ion(1).ge.0d0) m1 = mass_ion(1)
+         if (mass_ion(2).ge.0d0) m1 = mass_ion(2)
+         if(ebeam(1).lt.m1.and.lpp(1).ne.9) ebeam(1)=m1
+         if(ebeam(2).lt.m2.and.lpp(2).ne.9) ebeam(2)=m2
          pi1(0)=ebeam(1)
          pi1(3)=sqrt(max(ebeam(1)**2-m1**2,0d0))
          pi2(0)=ebeam(2)
