3ea6bdf9b48f4d03c5438968644d7be863a28732
Stefan Prestel prestel@slac.stanford.edu <>
2019-04-30 12:27:09 +0200

    adding pythia splitting information retrieval functions

--- Template/NLO/SubProcesses/pythia8_fortran.cc
+++ Template/NLO/SubProcesses/pythia8_fortran.cc
@@ -6,415 +6,17 @@
 using namespace std;
 using namespace Pythia8;
 
-// let us add a new class that inherits fomr LHAupFortran
-class MyLHAupFortran : public LHAupFortran_aMCatNLO {
-  public:
-
-  MyLHAupFortran(){
-    initialised = false;
-  }
-
-  //the common blocks should be alredy filled at the fortran level
-  //so simply return true
-  bool fillHepRup(){
-    initialised = true;
-    return true;
-  }
-  bool fillHepEup(){
-    return true;
-  }
-  
-  bool is_initialised(){
-    return initialised;
-  }
-
-  private:
-  bool initialised;
-};
-
-class PrintFirstEmission : public UserHooks {
-
-public:
-
-  PrintFirstEmission(LHA3FromPythia8* lhawriterPtrIn)
-    : lhawriterPtr(lhawriterPtrIn), inputEvent(50000) {
-    doRemoveDecayProducts=true;
-    inputEvent.init("(hard process-modified)", particleDataPtr);
-    resonances.resize(0);
-  }
-
-  bool canVetoISREmission() { return true; }
-  bool canVetoFSREmission() { return true; }
-
-  bool doVetoISREmission(int, const Event& event, int iSys) {
-    nISR++;
-    if (nISR + nFSR > 1) return false;
-    // Reattach resonance decay products and write event.
-    Event outEvent = makeHardEvent(iSys, event, true);
-    reattachResonanceDecays(outEvent);
-    lhawriterPtr->setEventPtr(&outEvent);
-    lhawriterPtr->setEvent();
-    // Done.
-    return false;
-  }
-
-  bool doVetoFSREmission(int, const Event& event, int iSys) {
-    nISR++;
-    if (nISR + nFSR > 1) return false;
-    // Reattach resonance decay products and write event.
-    Event outEvent = makeHardEvent(iSys, event, true);
-    reattachResonanceDecays(outEvent);
-    lhawriterPtr->setEventPtr(&outEvent);
-    lhawriterPtr->setEvent();
-    // Done.
-    return false;
-  }
-
-  bool canVetoProcessLevel() { return true; }
-  bool doVetoProcessLevel(Event& process) {
-    // Initailize and store resonance decay products.
-    nISR = nFSR = 0;
-    lhawriterPtr->setProcessPtr(&process);
-    bareEvent(process,true);
-    return false;
-  }
-
-  LHA3FromPythia8* lhawriterPtr;
-
-  Event inputEvent;
-  vector< pair<int,int> > resonances;
-  bool doRemoveDecayProducts;
-
-  int nISR, nFSR;
-
-  //--------------------------------------------------------------------------
-  Event makeHardEvent( int iSys, const Event& state, bool isProcess) {
-
-    bool hasSystems = !isProcess && partonSystemsPtr->sizeSys() > 0;
-    int sizeSys     = (hasSystems) ? partonSystemsPtr->sizeSys() : 1;
-
-    Event event = Event();
-    event.clear();
-    event.init( "(hard process-modified)", particleDataPtr );
-
-    int in1 = 0;
-    for ( int i = state.size()-1; i > 0; --i)
-      if ( state[i].mother1() == 1 && state[i].mother2() == 0
-        && (!hasSystems || partonSystemsPtr->getSystemOf(i,true) == iSys))
-        {in1 = i; break;}
-    if (in1 == 0) in1 = partonSystemsPtr->getInA(iSys);
-
-    int in2 = 0;
-    for ( int i = state.size()-1; i > 0; --i)
-      if ( state[i].mother1() == 2 && state[i].mother2() == 0
-        && (!hasSystems || partonSystemsPtr->getSystemOf(i,true) == iSys))
-        {in2 = i; break;}
-    if (in2 == 0) in2 = partonSystemsPtr->getInB(iSys);
-
-    // Try to find incoming particle in other systems, i.e. if the current
-    // system arose from a resonance decay.
-    bool resonantIncoming = false;
-    if ( in1 == 0 && in2 == 0 ) {
-      int iParentInOther = 0;
-      int nSys = partonSystemsPtr->sizeAll(iSys);
-      for (int iInSys = 0; iInSys < nSys; ++iInSys){
-        int iNow = partonSystemsPtr->getAll(iSys,iInSys);
-        for (int iOtherSys = 0; iOtherSys < sizeSys; ++iOtherSys){
-          if (iOtherSys == iSys) continue;
-          int nOtherSys = partonSystemsPtr->sizeAll(iOtherSys);
-          for (int iInOtherSys = 0; iInOtherSys < nOtherSys; ++iInOtherSys){
-            int iOtherNow = partonSystemsPtr->getAll(iOtherSys,iInOtherSys);
-            if (state[iNow].isAncestor(iOtherNow)) {
-              iParentInOther = iOtherNow;
-            }
-          }
-        }
-      }
-      in1 = iParentInOther;
-      if (iParentInOther) resonantIncoming = true;
-    } 
-
-    event.append(state[0]);
-    event.append(state[1]);
-    event[1].daughters(3,0);
-    event.append(state[2]);
-    event[2].daughters(4,0);
-
-    // Attach the first incoming particle.
-    event.append(state[in1]);
-    event[3].mothers(1,0);
-    if (resonantIncoming) event[3].status(-22);
-    else event[3].status(-21);
-
-    // Attach the second incoming particle.
-    event.append(state[in2]);
-    event[4].mothers(2,0);
-    event[4].status(-21);
-
-    for ( int i = 0; i < state.size(); ++i) {
-      // Careful when builing the sub-events: A particle that is currently
-      // intermediate in one system could be the pirogenitor of another
-      // system, i.e. when resonance decays are present. In this case, the
-      // intermediate particle in the current system should be final. 
-      bool isFin   = state[i].isFinal();
-      bool isInSys = (partonSystemsPtr->getSystemOf(i) == iSys);
-
-      bool isParentOfOther = false;
-      if (!isFin && isInSys) {
-        for (int iOtherSys = 0; iOtherSys < sizeSys; ++iOtherSys){
-          if (iOtherSys == iSys) continue;
-          double nSys = partonSystemsPtr->sizeAll(iOtherSys);
-          for (int iInSys = 0; iInSys < nSys; ++iInSys){
-            int iNow = partonSystemsPtr->getAll(iOtherSys,iInSys);
-            if (state[iNow].isAncestor(i)) {isParentOfOther=true; break;}
-          }
-        }
-      }
-
-      if ( (isFin || isParentOfOther) && (!hasSystems || isInSys) ) {
-      //if ( state[i].isFinal() 
-      //  && (!hasSystems || partonSystemsPtr->getSystemOf(i) == iSys)) {
-        int iN = event.append(state[i]);
-        event[iN].daughters(0,0);
-        event[iN].mothers(3,4);
-        event[iN].status(23);
-      }
-    }
-
-    // Set daughters of initial particles.
-    event[3].daughters(5,event.size()-1);
-    event[4].daughters(5,event.size()-1);
-    return event;
-
-  }
-
-
-  //--------------------------------------------------------------------------
-  // Return event stripped off decay products.
-  Event bareEvent(const Event& inputEventIn,
-    bool storeInputEvent ) {
-
-    // Find and detach decay products.
-    Event newProcess = Event();
-    newProcess.init("(hard process-modified)", particleDataPtr);
-
-    // If desired, store input event.
-    if ( storeInputEvent ) {
-      resonances.resize(0);
-      inputEvent.clear();
-      inputEvent = inputEventIn;
-    }
-
-    // Now remove decay products.
-    if ( doRemoveDecayProducts ) {
-
-      // Add the beam and initial partons to the event record.
-      for (int i = 0; i < inputEventIn.size(); ++ i) {
-        if ( inputEventIn[i].mother1() > 4
-          || inputEventIn[i].statusAbs() == 22
-          || inputEventIn[i].statusAbs() == 23)
-          break;
-        newProcess.append(inputEventIn[i]);
-      }
-
-      // Add the intermediate particles to the event record.
-      for (int i = 0; i < inputEventIn.size(); ++ i) {
-        if (inputEventIn[i].mother1() > 4) break;
-        if ( inputEventIn[i].status() == -22) {
-          int j = newProcess.append(inputEventIn[i]);
-          newProcess[j].statusPos();
-          if ( storeInputEvent ) resonances.push_back( make_pair(j, i) );
-          newProcess[j].daughters(0, 0);
-        }
-      }
-
-      // Add remaining outgoing particles to the event record.
-      for (int i = 0; i < inputEventIn.size(); ++ i) {
-        if (inputEventIn[i].mother1() > 4) break;
-        if ( inputEventIn[i].statusAbs() != 11
-          && inputEventIn[i].statusAbs() != 12
-          && inputEventIn[i].statusAbs() != 21
-          && inputEventIn[i].statusAbs() != 22)
-          newProcess.append(inputEventIn[i]);
-      }
-
-      // Update event colour tag to maximum in whole process.
-      int maxColTag = 0;
-      for (int i = 0; i < inputEventIn.size(); ++ i) {
-        if ( inputEventIn[i].col() > maxColTag )
-          maxColTag = inputEventIn[i].col();
-        if ( inputEventIn[i].acol() > maxColTag )
-          maxColTag = inputEventIn[i].acol();
-      }
-      newProcess.initColTag(maxColTag);
-
-      // Copy junctions from process to newProcess.
-      for (int i = 0; i < inputEventIn.sizeJunction(); ++i)
-        newProcess.appendJunction( inputEventIn.getJunction(i));
-
-      newProcess.saveSize();
-      newProcess.saveJunctionSize();
-
-    } else {
-      newProcess = inputEventIn;
-    }
-
-    // Remember scale
-    newProcess.scale( inputEventIn.scale() );
-
-    // Done
-    return newProcess;
-
-  }
-
-  //--------------------------------------------------------------------------
-  // Write event with decay products attached to argument. Only possible if an
-  // input event with decay producs had been stored before.
-  bool reattachResonanceDecays(Event& process ) {
-
-    // Now reattach the decay products.
-    if ( doRemoveDecayProducts && inputEvent.size() > 0 ) {
-
-    int sizeBef = process.size();
-    // Vector of resonances for which the decay products were already attached.
-    vector<int> iAftChecked;
-    // Reset daughters and status of intermediate particles.
-    for ( int i = 0; i < int(resonances.size()); ++i ) {
-      for (int j = 0; j < sizeBef; ++j ) {
-        if ( j != resonances[i].first ) continue;
-
-        int iOldDaughter1 = inputEvent[resonances[i].second].daughter1();
-        int iOldDaughter2 = inputEvent[resonances[i].second].daughter2();
-
-        // Get momenta in case of reclustering.
-        int iHardMother      = resonances[i].second;
-        Particle& hardMother = inputEvent[iHardMother];
-        // Find current mother copy (after clustering).
-        int iAftMother       = 0;
-        for ( int k = 0; k < process.size(); ++k )
-          if ( process[k].id() == inputEvent[resonances[i].second].id() ) {
-            // Only attempt if decays of this resonance were not attached.
-            bool checked = false;
-            for ( int l = 0; l < int(iAftChecked.size()); ++l )
-              if ( k == iAftChecked[l] )
-                checked = true;
-            if ( !checked ) {
-              iAftChecked.push_back(k);
-              iAftMother = k;
-              break;
-            }
-          }
-
-        Particle& aftMother  = process[iAftMother];
-
-        // Resonance can have been moved by clustering,
-        // so prepare to update colour and momentum information for system.
-        int colBef  = hardMother.col();
-        int acolBef = hardMother.acol();
-        int colAft  = aftMother.col();
-        int acolAft = aftMother.acol();
-        RotBstMatrix M;
-        M.bst( hardMother.p(), aftMother.p());
-
-        // Attach resonance decay products.
-        int iNewDaughter1 = 0;
-        int iNewDaughter2 = 0;
-        for ( int k = iOldDaughter1; k <= iOldDaughter2; ++k ) {
-          if ( k == iOldDaughter1 )
-            iNewDaughter1 = process.append(inputEvent[k] );
-          else
-            iNewDaughter2 = process.append(inputEvent[k] );
-          process.back().statusPos();
-          Particle& now = process.back();
-          // Update colour and momentum information.
-          if (now.col()  != 0 && now.col()  == colBef ) now.col(colAft);
-          if (now.acol() != 0 && now.acol() == acolBef) now.acol(acolAft);
-          now.rotbst( M);
-          // Update vertex information.
-          if (now.hasVertex()) now.vProd( aftMother.vDec() );
-          // Update mothers.
-          now.mothers(iAftMother,0);
-        }
-
-        process[iAftMother].daughters( iNewDaughter1, iNewDaughter2 );
-        process[iAftMother].statusNeg();
-
-        // Loop through event and attach remaining decays
-        int iDec = 0;
-        do {
-          if ( process[iDec].isFinal() && process[iDec].canDecay()
-            && process[iDec].mayDecay() && process[iDec].isResonance() ) {
-
-            int iD1 = process[iDec].daughter1();
-            int iD2 = process[iDec].daughter2();
-
-            // Done if no daughters exist.
-            if ( iD1 == 0 || iD2 == 0 ) continue;
-
-            // Attach daughters.
-            int iNewDaughter12 = 0;
-            int iNewDaughter22 = 0;
-            for ( int k = iD1; k <= iD2; ++k ) {
-              if ( k == iD1 )
-                iNewDaughter12 = process.append(inputEvent[k] );
-              else
-                iNewDaughter22 = process.append(inputEvent[k] );
-              process.back().statusPos();
-              Particle& now = process.back();
-              // Update colour and momentum information.
-              if (now.col() != 0 && now.col() == colBef ) now.col(colAft);
-              if (now.acol()!= 0 && now.acol()== acolBef) now.acol(acolAft);
-              now.rotbst( M);
-              // Update vertex information.
-              if (now.hasVertex()) now.vProd( process[iDec].vDec() );
-              // Update mothers.
-              now.mothers(iDec,0);
-            }
-
-            // Modify mother status and daughters.
-            process[iDec].status(-22);
-            process[iDec].daughters(iNewDaughter12, iNewDaughter22);
-
-          // End of loop over all entries.
-          }
-        } while (++iDec < process.size());
-      } // End loop over process entries.
-    } // End loop over resonances.
-
-    // Update event colour tag to maximum in whole process.
-    int maxColTag = 0;
-    for (int i = 0; i < process.size(); ++ i) {
-      if (process[i].col() > maxColTag) maxColTag = process[i].col();
-      if (process[i].acol() > maxColTag) maxColTag = process[i].acol();
-    }
-    process.initColTag(maxColTag);
-
-    }
-
-    // Done.
-    return (doRemoveDecayProducts) ? inputEvent.size() > 0 : true;
-
-  }
-
-};
-
-
-
-
 extern "C" { 
 
   // set up a global instance of pytia8
   Pythia pythia;
   // set up a global instance of LHAup
-  MyLHAupFortran lhareader;
+  //MyLHAupFortran lhareader;
+  MyLHAupFortran lhareader(&pythia.settings);
   LHA3FromPythia8 lhawriter(&pythia.event, &pythia.settings, &pythia.info,
     &pythia.particleData);
 
   PrintFirstEmission printFirstEmission(&lhawriter); 
-
-  //pythia.setUserHooksPtr(printFirstEmission);
-  //Pythia pythia2;
-  //pythia2.stat();
 
   // a counter for the number of event
   int iEvent = 0;
@@ -449,15 +51,49 @@ extern "C" {
   }
 
   // an initialisation function
-  void pythia_init_default_() {
+  void pythia_init_default_(int& idIn1, int& idIn2, int outIDs [10], double masses[26]) {
     lhareader.setInit();
     // Example of a user hook for storing in the out stream the event after the first emission.
     pythia.setUserHooksPtr(&printFirstEmission);
+
+    // Reconstruct the process string.
+    string processString = "";
+    // Set incoming particles.
+    if (idIn1 == 2212) processString += "p";
+    if (idIn1 == 11)   processString += "e-";
+    if (idIn1 ==-11)   processString += "e+";
+    if (idIn2 == 2212) processString += "p";
+    if (idIn2 == 11)   processString += "e-";
+    if (idIn2 ==-11)   processString += "e+";
+    processString += ">";
+    // Set outgoing particles.
+    bool foundOutgoing = false;
+    for (int i=0; i < 10; ++i) {
+      if (outIDs[i]==0) continue;
+      if (outIDs[i]==2212) {
+        processString += "j";
+      } else {
+        ostringstream proc;
+        proc << "{" << pythia.particleData.name(outIDs[i]) << "," << outIDs[i] << "}";
+        processString += proc.str();
+      }
+    }
+
+    // Initialize masses.
+    for (int i=1; i <= 25; ++i){
+      if (masses[i]<0.) continue;
+      stringstream s;
+      // Need a non-zero muon mass to get correct Higgs width. Otherwise gets a NAN. Need to be fixed later.
+      if (i==13) continue;
+      s << i << ":m0 =" << masses[i];
+      pythia.readString(s.str());
+    }
+
     cout<<"Using default initialization of Pythia8."<<endl;
     pythia.readString("Beams:frameType=5");
     pythia.readString("Check:epTolErr=1.0000000000e-02");
     pythia.readString("merging:doptlundmerging = on");
-    pythia.readString("merging:process = pp>LEPTONS,NEUTRINOS");
+    pythia.settings.word("Merging:Process", processString);
     pythia.readString("merging:tms = 1000000");
     pythia.readString("merging:includeWeightInXSection = off");
     pythia.readString("merging:njetmax = 1000");
@@ -467,6 +103,12 @@ extern "C" {
     pythia.readString("LesHouches:setLeptonMass = 0");
     pythia.readString("LesHouches:mRecalculate = -1.0");
     pythia.readString("LesHouches:matchInOut = off");
+    pythia.readString("PartonLevel:MPI                 = off");
+    pythia.readString("Print:quiet = on");
+    pythia.readString("Merging:nRequested = 0");
+    pythia.readString("Beams:setProductionScalesFromLHEF = off");
+    pythia.readString("Check:abortIfVeto               = on");
+    pythia.readString("Merging:mayRemoveDecayProducts  = on");
 
     pythia.setLHAupPtr(& lhareader);
     pythia.init();
@@ -494,11 +136,16 @@ extern "C" {
     ++iEvent;
   }
 
-    //This should set the LHA event using fortran common blocks
-  //a function to close everything
-  void pythia_stat_() {
-    pythia.stat();
+  void pythia_get_stopping_info_( double scales [100][100],
+    double mass [100][100] ) {
+    pythia.mergingPtr->getStoppingInfo(scales, mass);
   }
+
+  void pythia_get_dead_zones_( bool dzone [100][100] ) {
+    pythia.mergingPtr->getDeadzones( dzone);
+  }
+
+  void pythia_clear_() { pythia.mergingPtr->clear(); }
 
 }
 
