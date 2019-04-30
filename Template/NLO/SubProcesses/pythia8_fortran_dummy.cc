18f3c045bc36255db4adac3ab5cc09c9d82a5b4c
Stefan Prestel prestel@slac.stanford.edu <>
2019-04-30 12:26:36 +0200

    adding pythia splitting information retrieval functions

--- Template/NLO/SubProcesses/pythia8_fortran_dummy.cc
+++ Template/NLO/SubProcesses/pythia8_fortran_dummy.cc
@@ -1,26 +1,17 @@
 extern "C" { 
 
-  // an initialisation function
   void pythia_init_(char input[500]) {}
   void pythia_init_default_() {}
-
-  // a function to shower and analyse events
   void pythia_setevent_() {}
-
-  // a function to shower and analyse events
   void pythia_next_() {}
-
-    //This should set the LHA event using fortran common blocks
-  //a function to close everything
-  void pythia_stat_() {}
+  void pythia_get_stopping_info_( double scales [100][100], double mass [100][100] ) {}
+  void pythia_get_dead_zones_( bool dzone [100][100] ) {}
+  void pythia_clear_() {}
 
   void dire_init_(char input[500]) {}
   void dire_init_default_() {}
   void dire_setevent_() {}
   void dire_next_() {}
-  void dire_stat_() {}
-  void dire_get_mergingweight_(double& w) {}
-  void dire_get_sudakov_stopping_scales_( double scales [1000] ) {}
   void dire_get_stopping_info_( double scales [100][100], double mass [100][100] ) {}
   void dire_get_dead_zones_( bool dzone [100][100] ) {}
   void dire_clear_() {}
