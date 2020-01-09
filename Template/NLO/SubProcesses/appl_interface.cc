a6e5375612e7848751b2f5accce3de6b44c00564
Christopher Schwan <handgranaten-herbert@posteo.de>
2020-01-09 17:44:29 +0100

    Fix binning problem when running with more than one NLO

--- Template/NLO/SubProcesses/appl_interface.cc
+++ Template/NLO/SubProcesses/appl_interface.cc
@@ -384,16 +384,14 @@ extern "C" void appl_fill_() {
     // Get Bjorken x's
     x1     = appl_common_weights_.x1[0];
     x2     = appl_common_weights_.x2[0];
-    static std::vector<double> x1Saved(grid_obs.size(), 0.0);
-    static std::vector<double> x2Saved(grid_obs.size(), 0.0);
-    static int grid_index_saved = -1;
-    if(x1 == x1Saved[nh] && x2 == x2Saved[nh] && (grid_index_saved == grid_index))
+    static std::vector<std::vector<double>> x1Saved(grid_obs.size(), std::vector<double>(grid_obs[nh].order_ids().size(), 0.0));
+    static std::vector<std::vector<double>> x2Saved(grid_obs.size(), std::vector<double>(grid_obs[nh].order_ids().size(), 0.0));
+    if(x1 == x1Saved[nh][grid_index] && x2 == x2Saved[nh][grid_index])
       return;
     else
     {
-      x1Saved[nh] = x1;
-      x2Saved[nh] = x2;
-      grid_index_saved = grid_index;
+      x1Saved[nh][grid_index] = x1;
+      x2Saved[nh][grid_index] = x2;
     }
     // Energy scale
     scale2 = appl_common_weights_.muF2[0];
@@ -424,16 +422,14 @@ extern "C" void appl_fill_() {
       x2     = appl_common_weights_.x2[k];
       if(k == 1)
       {
-        static std::vector<double> x1Saved(grid_obs.size(), 0.0);
-        static std::vector<double> x2Saved(grid_obs.size(), 0.0);
-        static int grid_index_saved = -1;
-        if(x1 == x1Saved[nh] && x2 == x2Saved[nh] && (grid_index_saved == grid_index))
+        static std::vector<std::vector<double>> x1Saved(grid_obs.size(), std::vector<double>(grid_obs[nh].order_ids().size(), 0.0));
+        static std::vector<std::vector<double>> x2Saved(grid_obs.size(), std::vector<double>(grid_obs[nh].order_ids().size(), 0.0));
+        if(x1 == x1Saved[nh][grid_index] && x2 == x2Saved[nh][grid_index])
           return;
         else
         {
-          x1Saved[nh] = x1;
-          x2Saved[nh] = x2;
-          grid_index_saved = grid_index;
+          x1Saved[nh][grid_index] = x1;
+          x2Saved[nh][grid_index] = x2;
         }
       }
       scale2 = appl_common_weights_.muF2[k];
@@ -466,16 +462,14 @@ extern "C" void appl_fill_() {
   else if(itype == 3) {
     x1     = appl_common_weights_.x1[1];
     x2     = appl_common_weights_.x2[1];
-    static std::vector<double> x1Saved(grid_obs.size(), 0.0);
-    static std::vector<double> x2Saved(grid_obs.size(), 0.0);
-    static int grid_index_saved = -1;
-    if(x1 == x1Saved[nh] && x2 == x2Saved[nh] && (grid_index_saved == grid_index))
+    static std::vector<std::vector<double>> x1Saved(grid_obs.size(), std::vector<double>(grid_obs[nh].order_ids().size(), 0.0));
+    static std::vector<std::vector<double>> x2Saved(grid_obs.size(), std::vector<double>(grid_obs[nh].order_ids().size(), 0.0));
+    if(x1 == x1Saved[nh][grid_index] && x2 == x2Saved[nh][grid_index])
       return;
     else
     {
-      x1Saved[nh] = x1;
-      x2Saved[nh] = x2;
-      grid_index_saved = grid_index;
+      x1Saved[nh][grid_index] = x1;
+      x2Saved[nh][grid_index] = x2;
     }
     scale2 = appl_common_weights_.muF2[1];
     ilumi  = appl_common_weights_.flavmap[1] - 1;
