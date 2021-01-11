4a806ab9d007f0da8b90ea05cc4dc55e8c3ed33d
olivier-mattelaer <>
2021-01-11 13:50:06 +0100

    another encoding issue

--- madgraph/various/misc.py
+++ madgraph/various/misc.py
@@ -1590,7 +1590,7 @@ class digest:
         def digest(text):
             """using mg5 for the hash"""
             t = hashlib.md5()
-            t.update(text)
+            t.update(text.encode())
             return t.hexdigest()
         return digest
     
