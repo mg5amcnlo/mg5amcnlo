c5b6696f12a9d63a01d48a103503d4048ef62ba9
Davide Pagani <davide.pagani@tum.de>
2021-02-19 17:59:15 +0100

    SC_CR.sh adapted for last push

--- Template/NLO/SubProcesses/SC_CR.sh
+++ Template/NLO/SubProcesses/SC_CR.sh
@@ -7,6 +7,7 @@ do
   cd $x
   ln -s ../input_check_sudakov.dat
   cp V0*/nsqso_born.inc .
+  cp V0*/nsquaredSO.inc .
   make check_sudakov > check_sudakov_make.log
   ./check_sudakov < input_check_sudakov.dat > output_check_sudakov.dat 
   cd $basedir
