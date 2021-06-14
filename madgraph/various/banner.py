e62b35f90288ceda9d741a549ec1d0f3b828dfd4
Anton Safronov <safronov@ipno.in2p3.fr>
2021-06-14 17:39:06 +0200

    ACHIEVEMENT

--- madgraph/various/banner.py
+++ madgraph/various/banner.py
@@ -4129,7 +4129,12 @@ class RunCardNLO(RunCard):
         self.add_param('lpp1', 1, fortran_name='lpp(1)')        
         self.add_param('lpp2', 1, fortran_name='lpp(2)')                        
         self.add_param('ebeam1', 6500.0, fortran_name='ebeam(1)')
-        self.add_param('ebeam2', 6500.0, fortran_name='ebeam(2)')        
+        self.add_param('ebeam2', 6500.0, fortran_name='ebeam(2)')
+
+##############Anton
+        self.add_param('RPA_choice', False, fortran_name='RPA_choice')#,allowed=['False', 'True'])
+##############Anton
+
         self.add_param('pdlabel', 'nn23nlo', allowed=['lhapdf', 'cteq6_m','cteq6_d','cteq6_l','cteq6l1', 'nn23lo','nn23lo1','nn23nlo','ct14q00','ct14q07','ct14q14','ct14q21'])                
         self.add_param('lhaid', [244600],fortran_name='lhaPDFid')
         self.add_param('lhapdfsetname', ['internal_use_only'], system=True)
