3d0babb7c86f943b40fdc9cd7a500dd33fb517b2
Marco Zaro <marco.zaro@gmail.com>
2021-12-03 15:49:55 +0100

    orderstag_glob is now determined by the python code, not read from logs

--- madgraph/interface/amcatnlo_run_interface.py
+++ madgraph/interface/amcatnlo_run_interface.py
@@ -2023,6 +2023,7 @@ class aMCatNLOCmd(CmdExtended, HelpToCmd, CompleteForCmd, common_run.CommonRunCm
             event_norm=self.run_card['event_norm']
             # gather the various orders tag and write include files (only needed once)
             if not os.path.exists(pjoin(self.me_dir, 'SubProcesses', 'orderstags_glob.dat')):
+                raise aMCatNLOError('Error, cannot find orderstag_glob.dat')
                 self.write_orders_tag_info()
 
             return self.reweight_and_collect_events(options, mode, nevents, event_norm)
