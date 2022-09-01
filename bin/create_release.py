9b892f7a3910ea629c0afbe303ba056dc2031d2e
Olivier Mattelaer <olivier.mattelaer@uclouvain.be>
2022-09-01 12:33:22 +0200

    update creation script

--- bin/create_release.py
+++ bin/create_release.py
@@ -198,7 +198,7 @@ shutil.move(path.join(filepath, 'README.release'), path.join(filepath, 'README')
 # 1. Add information for the auto-update
 if rev_nb:
     fsock = open(os.path.join(filepath,'input','.autoupdate'),'w')
-    fsock.write("version_nb   %s\n" % rev_nb)
+    fsock.write("version_nb   %s\n" % int(rev_nb))
     fsock.write("last_check   %s\n" % int(time.time()))
     fsock.write("last_message %s\n" % int(last_message))
     fsock.close()
