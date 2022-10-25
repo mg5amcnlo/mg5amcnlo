b98ef3dd99398a3b90dce79eb4b81bb5fd5d2fbe
Olivier Mattelaer <olivier.mattelaer@uclouvain.be>
2022-10-25 21:51:44 +0200

    fix an issue with complex mass scheme

--- madgraph/core/base_objects.py
+++ madgraph/core/base_objects.py
@@ -1479,7 +1479,7 @@ class Model(PhysicsObject):
         # recast all parameter in prefix_XX
         for key in keys:
             for param in self['parameters'][key]:
-                value = param.name.lower()
+                value = param.name.lower()   
                 if value in ['as','mu_r', 'zero','aewm1','g']:
                     continue
                 elif value.startswith(prefix):
@@ -1516,13 +1516,12 @@ class Model(PhysicsObject):
             self.map_CTcoup_CTparam = dict( (coup_name, 
             [change[name] if (name in change) else name for name in params]) 
                   for coup_name, params in self.map_CTcoup_CTparam.items() )
-
+        
         i=0
         while i*1000 <= len(to_change): 
             one_change = to_change[i*1000: min((i+1)*1000,len(to_change))]
             i+=1
             rep_pattern = re.compile('\\b%s\\b'% (re_expr % ('\\b|\\b'.join(one_change))))
-            
             # change parameters
             for key in keys:
                 if key == ('external',):
@@ -1547,7 +1546,7 @@ class Model(PhysicsObject):
                 if str(part.get('width')) in one_change:
                     part.set('width', rep_pattern.sub(replace, str(part.get('width'))))  
                 if  hasattr(part, 'partial_widths'):
-                    for key, value in part.partial_widths.items():    
+                    for key, value in part.partial_widths.items():
                         part.partial_widths[key] = rep_pattern.sub(replace, value)
                 
         #ensure that the particle_dict is up-to-date
@@ -1678,6 +1677,7 @@ class Model(PhysicsObject):
                 return True
             else:
                 return False
+            
 
     def change_mass_to_complex_scheme(self, toCMS=True, bypass_check=False):
         """modify the expression changing the mass to complex mass scheme"""
@@ -1736,13 +1736,13 @@ class Model(PhysicsObject):
                 if particle.get('pdg_code') == 24 and isinstance(mass, 
                                                                  ModelVariable):
                     status = self.change_electroweak_mode(
-                                                   set(['mz','mw','alpha']), bypass_check)
+                                                   set(['mz','mw','alpha']), bypass_check=bypass_check)
                     # Use the newly defined parameter for the W mass
                     mass = self.get_parameter(particle.get('mass'))
                     if not status:
                         logger.warning('The W mass is not an external '+
                         'parameter in this model and the automatic change of'+
-                        ' electroweak scheme changed. This is not advised for '+
+                        ' electroweak scheme failed. This is not advised for '+
                                             'applying the complex mass scheme.')
 
                 # Add A new parameter CMASS
