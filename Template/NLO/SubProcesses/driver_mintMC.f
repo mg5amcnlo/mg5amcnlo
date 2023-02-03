8ec28e37c7d69f63b1cd3e93d86de8af99f280a7
Olivier Mattelaer <olivier.mattelaer@uclouvain.be>
2023-02-03 21:41:39 +0100

    more change to file opening

--- Template/NLO/SubProcesses/driver_mintMC.f
+++ Template/NLO/SubProcesses/driver_mintMC.f
@@ -94,6 +94,8 @@ c general MadFKS parameters
       include "FKSParams.inc"
       logical              fixed_order,nlo_ps
       common /c_fnlo_nlops/fixed_order,nlo_ps
+c     local
+      logical fopened
 
 C-----
 C  BEGIN CODE
@@ -189,7 +191,7 @@ c initialize grids
             enddo
          else
 c to restore grids:
-            open (unit=12, file='mint_grids',status='old')
+            call open_file (12, 'mint_grids', fopened)
             do j=0,nintervals
                read (12,*) (xgrid(j,i,1),i=1,ndim)
             enddo
@@ -235,7 +237,7 @@ c     computation of upper bounding envelope
 c*************************************************************
       elseif(imode.eq.1) then
 c to restore grids:
-         open (unit=12, file='mint_grids',status='old')
+         call open_file_local(12, 'mint_grids', fopened)
          do j=0,nintervals
             read (12,*) (xgrid(j,i,1),i=1,ndim)
          enddo
@@ -328,7 +330,7 @@ c Mass-shell stuff. This is MC-dependent
          ncall=nevts ! Update ncall with the number found in 'nevts'
 
 c to restore grids:
-         open (unit=12, file='mint_grids',status='unknown')
+         call open_file_local (12, 'mint_grids', fopened)
          read (12,*) (xgrid(0,i,1),i=1,ndim)
          do j=1,nintervals
             read (12,*) (xgrid(j,i,1),i=1,ndim)
@@ -1304,3 +1306,82 @@ c     include all quarks (except top quark) and the gluon.
       endif
       return
       end
+
+            subroutine open_file_local(lun,filename,fopened)
+c***********************************************************************
+c     opens file input-card.dat in current directory or above
+c***********************************************************************
+      implicit none
+      include 'nexternal.inc'
+      include 'mint.inc'
+c
+c     Arguments
+c
+      integer lun
+      logical fopened
+      character*(*) filename
+      character*300  tempname
+      character*300  tempname2
+      character*300 path ! path of the executable
+      character*30  upname ! sequence of ../
+      character*30 buffer,buffer2
+      integer fine,fine2
+      integer i, pos
+
+c-----
+c  Begin Code
+c-----
+c
+c     first check that we will end in the main directory
+c
+
+c
+cv    check local file
+c
+      fopened=.false.
+      tempname=filename 	 
+      fine=index(tempname,' ') 	 
+      fine2=index(path,' ')-1	 
+      if(fine.eq.0) fine=len(tempname)
+      open(unit=lun,file=tempname,status='old',ERR=20)
+      fopened=.true.
+      write(*,*) "FOUND LOCALLY"
+      return
+
+c      
+c     getting the path of the executable
+c
+ 20   call getarg(0,path) !path is the PATH to the madevent executable (either global or from launching directory)
+      pos = index(path,'/', .true.)
+      path = path(:pos)
+      fine2 = index(path, ' ')-1
+c
+c     getting the name of the directory
+c
+c      if (lbw(0).eq.0)then
+         ! No BW separation
+         write(buffer,*)  iconfig
+         path = path(:fine2)//'GF'//adjustl(buffer)
+         fine2 = index(path, ' ') -1
+
+         write(*,*) "search path", path, fine2, filename
+c      else
+c         ! BW separation
+c         call Encode(jconfig,lbw(1),3,nexternal)
+c         write(buffer,*) mincfig
+c         buffer = adjustl(buffer)
+c         fine = index(buffer, ' ')-1
+c         write(buffer2,*) jconfig
+c         buffer2=adjustl(buffer2)
+c         path = path(:fine2)//'G'//buffer(:fine)//'.'//buffer2
+c         fine2 = index(path, ' ')-1
+c      endif
+         tempname = path(:fine2)//'/'//filename
+         write(*,*) 'search tempname', tempname
+      open(unit=lun,file=tempname,status='old',ERR=30)
+      fopened = .true.
+      
+ 30    return
+       end
+
+
