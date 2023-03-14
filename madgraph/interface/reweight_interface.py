3e112084b5597ebf981ec044560ba01b23c58e1b
Olivier Mattelaer <olivier.mattelaer@uclouvain.be>
2023-03-14 21:38:41 +0100

    fix issue with reweight_name

--- madgraph/interface/reweight_interface.py
+++ madgraph/interface/reweight_interface.py
@@ -693,9 +693,11 @@ class ReweightInterface(extended_cmd.Cmd):
                 self.all_cross_section[(tag_name,name)] = (cross[name], error[name])
 
         # perform the scanning
-        reweight_name = self.options['rwgt_name'].rsplit('_',1)[0] # to avoid side effect during the scan
         if param_card_iterator:
-            reweight_name = self.options['rwgt_name'].rsplit('_',1)[0] # to avoid side effect during the scan
+            if self.options['rwgt_name']:
+                reweight_name = self.options['rwgt_name'].rsplit('_',1)[0] # to avoid side effect during the scan
+            else:
+                reweight_name = None
             for i,card in enumerate(param_card_iterator):
                 if reweight_name:
                     self.options['rwgt_name'] = '%s_%s' % (reweight_name, i+1)
