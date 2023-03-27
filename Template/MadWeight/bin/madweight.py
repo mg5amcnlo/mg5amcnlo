8fdc269f1013c807bf347d34f147b1da3092559e
Bruno Miguel Oliveira <129113364+Bruno-Miguel-Oliveira@users.noreply.github.com>
2023-03-27 23:00:50 +0100

    Change history files (mg5history, me5history, mw5history, decayhistory) from $HOME/.mg5/ to $XDG_STATE_HOME/mg5/, unless $HOME/.mg5/ already exists.
    According to the XDG Base Directory specification (https://specifications.freedesktop.org/basedir-spec/basedir-spec-latest.html):
    > The $XDG_STATE_HOME contains state data that should persist between (application) restarts, but that is not important or portable enough to the user that it should be stored in $XDG_DATA_HOME. It may contain:
    >
    >    actions history (logs, history, recently used files, …)
    >
    >    current state of the application that can be reused on a restart (view, layout, open files, undo history, …)

--- Template/MadWeight/bin/madweight.py
+++ Template/MadWeight/bin/madweight.py
@@ -3,11 +3,11 @@
 #
 # Copyright (c) 2011 The MadGraph Development team and Contributors
 #
-# This file is a part of the MadGraph 5 project, an application which 
+# This file is a part of the MadGraph 5 project, an application which
 # automatically generates Feynman diagrams and matrix elements for arbitrary
 # high-energy processes in the Standard Model and beyond.
 #
-# It is subject to the MadGraph license which should accompany this 
+# It is subject to the MadGraph license which should accompany this
 # distribution.
 #
 # For more information, please visit: http://madgraph.phys.ucl.ac.be
@@ -19,7 +19,7 @@ from __future__ import absolute_import
 from __future__ import print_function
 import sys
 import os
-import subprocess 
+import subprocess
 import logging
 import logging.config
 import re
@@ -39,14 +39,11 @@ if __debug__ and (not os.path.exists(os.path.join(root_path,'../..', 'bin','crea
     subprocess.call([sys.executable] + ['-O'] + sys.argv)
     sys.exit()
 
-
-
-
 pjoin = os.path.join
 sys.path.append(pjoin(root_path,'bin','internal'))
-import madweight_interface as MW        
+import madweight_interface as MW
 
-try: 
+try:
     import readline
 except ImportError:
     try:
@@ -58,8 +55,8 @@ else:
 
     if 'r261:67515' in sys.version and  'GCC 4.2.1 (Apple Inc. build 5646)' in sys.version:
         readline.parse_and_bind("bind ^I rl_complete")
-        readline.__doc__ = 'libedit'  
-    
+        readline.__doc__ = 'libedit'
+
     elif hasattr(readline, '__doc__'):
         if 'libedit' not in readline.__doc__:
             readline.parse_and_bind("tab: complete")
@@ -68,11 +65,19 @@ else:
     else:
         readline.__doc__ = 'GNU'
         readline.parse_and_bind("tab: complete")
-        
+
     # charge history file
     try:
-        history_file = os.path.join(os.environ['HOME'], '.mg5', 'mw5history')
+        legacy_state_dir = os.path.join(os.environ['HOME'], '.mg5')
+
+        if os.path.exists(legacy_state_dir):
+            state_dir = legacy_state_dir
+        else:
+            state_dir = os.getenv('XDG_STATE_HOME', os.path.join(os.environ['HOME'], '.local', 'state'))
+
+        history_file = os.path.join(state_dir, "mw5history")
         readline.read_history_file(history_file)
+
     except:
         pass
 
@@ -85,14 +90,12 @@ except:
 if __debug__:
         print('Running MG5 in debug mode')
 
-
 def set_configuration():
     import coloring_logging
     logging.config.fileConfig(os.path.join(root_path, 'bin', 'internal', 'me5_logging.conf'))
     logging.root.setLevel(logging.INFO)
     logging.getLogger('madevent').setLevel(logging.INFO)
-    logging.getLogger('madgraph').setLevel(logging.INFO)    
-
+    logging.getLogger('madgraph').setLevel(logging.INFO)
 
 def treat_old_argument(argument):
     """Have the MW4 behavior for this script"""
@@ -104,7 +107,7 @@ def treat_old_argument(argument):
         MWparam.set_run_opt(sys.argv)
     logger = logging.getLogger('madgraph')
     opt =  MWparam.run_opt
-    cmds = [] 
+    cmds = []
     if opt['param']: #-1
         cmds.append('treatcards')
     if opt['analyzer']: #-2
@@ -120,11 +123,11 @@ def treat_old_argument(argument):
     if opt['control']: # -7
         logger.warning('Option -7/control not supported anymore.')
     if opt['status']: # -7
-        logger.warning('Option "status" not supported anymore.')        
+        logger.warning('Option "status" not supported anymore.')
     if opt['collect']: # -8
-        cmds.append('collect')   
+        cmds.append('collect')
     if opt['plot']: # -9
-        logger.warning('Option -9/plot not supported anymore.')            
+        logger.warning('Option -9/plot not supported anymore.')
     if opt['relaunch']: # relaunch
         cmds.append('refine 1')
     if opt['refine']: # refine
@@ -132,18 +135,13 @@ def treat_old_argument(argument):
     if opt['clean'] == 1: # clean
          cmds.append('clean')
     if opt['clean']: # clean
-         cmds.append('clean %s' % opt['clean'])         
-         
+         cmds.append('clean %s' % opt['clean'])
+
     return cmds
 
-
-
-
-
-
-################################################################################  
+################################################################################
 ##   EXECUTABLE
-################################################################################                                
+################################################################################
 if '__main__' == __name__:
     # Check that python version is valid
 
@@ -157,7 +155,7 @@ if '__main__' == __name__:
             cmds = treat_old_argument(argument)
             for cmd in cmds:
                 launch.run_cmd(cmd)
-        else: 
+        else:
             launch.run_cmd('launch %s' % ' '.join(argument[1:]))
             launch.run_cmd('quit')
     except KeyboardInterrupt:
@@ -169,26 +167,12 @@ if '__main__' == __name__:
         logging.error(str(error))
         sys.exit()
     except Exception as error:
-        if os.path.exists(pjoin(root_path, 'RunWeb')): 
+        if os.path.exists(pjoin(root_path, 'RunWeb')):
             os.remove(pjoin(root_path, 'RunWeb'))
-        raise 
-                        
-    if os.path.exists(pjoin(root_path, 'RunWeb')): 
-        os.remove(pjoin(root_path, 'RunWeb'))      
-    
-    
-    # reconfigure path for the web 
+        raise
+
+    if os.path.exists(pjoin(root_path, 'RunWeb')):
+        os.remove(pjoin(root_path, 'RunWeb'))
+    # reconfigure path for the web
     #if len(argument) == 5:
     #    ME.pass_in_web_mode()
-
-             
-        
-
-        
-    
-    
-    
-    
-    
-    
-    
