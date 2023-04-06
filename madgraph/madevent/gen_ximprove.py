95b5b3975b11f6ce96b89742d564cc0e48d9d4ff
Ramon Winterhalder <rpw@mailbox.org>
2023-04-06 14:33:17 +0200

    Update makefile

--- madgraph/madevent/gen_ximprove.py
+++ madgraph/madevent/gen_ximprove.py
@@ -371,7 +371,7 @@ class gensym(object):
             # Compile madevent      
             self.cmd.compile(['madevent'], cwd=Pdir)
             # Compile madevent-api
-            self.cmd.compile(['madevent_vec.so'], cwd=Pdir)
+            self.cmd.compile(['madnis'], cwd=Pdir)
             if to_submit:
                 self.submit_to_cluster(job_list)
                 job_list = {}
