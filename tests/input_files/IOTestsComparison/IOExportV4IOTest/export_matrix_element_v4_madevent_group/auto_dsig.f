4d92a4d6efba0a1b988f607d7b9c371be719bd42
Olivier Mattelaer <olivier.mattelaer@uclouvain.be>
2023-04-28 17:01:18 +0200

    update IOTest

--- tests/input_files/IOTestsComparison/IOExportV4IOTest/export_matrix_element_v4_madevent_group/auto_dsig.f
+++ tests/input_files/IOTestsComparison/IOExportV4IOTest/export_matrix_element_v4_madevent_group/auto_dsig.f
@@ -81,10 +81,11 @@ C     Keep track of whether cuts already calculated for this event
       COMMON/TO_SUB_DIAG/SUBDIAG,IB
       INCLUDE 'coupl.inc'
       INCLUDE 'run.inc'
+      INCLUDE '../../Source/PDF/pdf.inc'
 C     Common blocks
-      CHARACTER*7         PDLABEL,EPA_LABEL
-      INTEGER       LHAID
-      COMMON/TO_PDF/LHAID,PDLABEL,EPA_LABEL
+C     CHARACTER*7         PDLABEL,EPA_LABEL
+C     INTEGER       LHAID
+C     COMMON/TO_PDF/LHAID,PDLABEL,EPA_LABEL
 C     
 C     local
 C     
