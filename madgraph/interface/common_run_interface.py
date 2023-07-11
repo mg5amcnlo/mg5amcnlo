5de033bb1a0bc30edd148dfaa56a93fb9a2e0686
Olivier Mattelaer <olivier.mattelaer@uclouvain.be>
2023-07-11 01:52:54 +0100

    fix some issue with contur

--- madgraph/interface/common_run_interface.py
+++ madgraph/interface/common_run_interface.py
@@ -2917,7 +2917,7 @@ class CommonRunCmd(HelpToCmd, CheckValidForCmd, cmd.Cmd):
                 raise Exception("contur_path is not correctly setup. Should be the directory of contur and conturenv.sh script")
             #4 Write Rivet lines
             shell = 'bash' if misc.get_shell_type() in ['bash',None] else 'tcsh'
-            shell = misch.which(shell)
+            shell = misc.which(shell)
             p = subprocess.Popen('source {0} &>/dev/null; echo $CONTUR_USER_DIR'.format(pjoin(self.options['contur_path'], "conturenv.sh"))
                                  , shell=True, stdout=subprocess.PIPE, executable=shell)
             (out,_) = p.communicate()                            
