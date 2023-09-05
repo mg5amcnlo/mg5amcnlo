7e9b132d965c5625fce67e13f1d70ee084d11eb6
Ramon Winterhalder <rpw@mailbox.org>
2023-09-05 16:12:54 +0200

    Add x1 and x2 output to the cpp wrapper for batching

--- Template/LO/SubProcesses/madnis.cpp
+++ Template/LO/SubProcesses/madnis.cpp
@@ -7,7 +7,7 @@
 extern "C" {
     void configure_code_cc_(bool, bool, double&); 
     void madnis_api_(double*, int&, int&, bool&, double&);
-    void get_momenta_(double*);
+    void get_momenta_(double*, double*, double*);
     void get_multichannel_(double*, int*);
 }
 
@@ -20,10 +20,8 @@ double Weight(double *rans, int ndim, int channel) {
 }
 
 // momenta
-std::vector<double> Momenta(int npart) {
-    std::vector<double> momenta(npart*4);
-    get_momenta_(momenta.data());
-    return momenta;
+void GetMomenta(std::vector<double>* momenta, double* x1, double* x2) {
+    get_momenta_(momenta->data(), x1, x2);
 }
 
 // alpha
@@ -45,7 +43,9 @@ extern "C" void call_madgraph(
     int npart,
     int nchan, 
     double* w_out, 
-    double* mom_out, 
+    double* mom_out,
+    double* x1_out,
+    double* x2_out,
     double* alpha_out,
     int* used_channels_out){
     for (int ibatch = 0; ibatch < nbatch; ibatch++) {
@@ -53,10 +53,16 @@ extern "C" void call_madgraph(
         double weight = Weight(z + ibatch*ndim, ndim, channel + 1);
 
         // Get momenta
-        auto mom = Momenta(npart);
+        double x1;
+        double x2;
+        std::vector<double> momenta(npart*4);
+        GetMomenta(&momenta, &x1, &x2);
         for(size_t imom = 0; imom < 4*npart; ++imom){
-            mom_out[ibatch*4*npart + imom] = mom[imom];
+            mom_out[ibatch*4*npart + imom] = momenta[imom];
         }
+
+        x1_out[ibatch] = x1;
+        x2_out[ibatch] = x2;
 
         // Get corresponding alpha and make sure its not NaN
         std::vector<double> alpha(nchan);
