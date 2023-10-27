769c0823d92785cfd26b0a008795d616f5195033
Olivier Mattelaer <olivier.mattelaer@uclouvain.be>
2023-10-27 09:19:51 +0200

    avoid test that are crashing if gs is not installed + fixed test for auto_convert_model

--- tests/acceptance_tests/test_cmd.py
+++ tests/acceptance_tests/test_cmd.py
@@ -216,7 +216,7 @@ class TestCmdShell1(unittest.TestCase):
                     'samurai': None,
                     'max_t_for_channel': 99,
                     'zerowidth_tchannel': True,
-                     'auto_convert_model': False,
+                     'auto_convert_model': True,
                      'nlo_mixed_expansion': True,
                      'acknowledged_v3.1_syntax': False,
                      'contur_path': './HEPTools/contur',
@@ -1072,11 +1072,12 @@ C
         self.assertTrue(os.path.exists(os.path.join(self.out_dir,
                                                     'SubProcesses',
                                                     'P2_gg_qq')))
-        self.assertTrue(os.path.exists(os.path.join(self.out_dir,
+        if misc.which('gs'):
+            self.assertTrue(os.path.exists(os.path.join(self.out_dir,
                                                     'SubProcesses',
                                                     'P2_gg_qq',
                                                     'matrix11.jpg')))
-        self.assertTrue(os.path.exists(os.path.join(self.out_dir,
+            self.assertTrue(os.path.exists(os.path.join(self.out_dir,
                                                     'HTML',
                                                     'card.jpg')))
         # Check that the run_config.inc file has been modified correctly
