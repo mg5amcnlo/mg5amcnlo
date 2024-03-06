e339df2f42edbc0b174e54d7fc7475a85bfbb8fa
Olivier Mattelaer <olivier.mattelaer@uclouvain.be>
2024-03-06 14:14:16 +0100

    fix for pythia8

--- madgraph/interface/madevent_interface.py
+++ madgraph/interface/madevent_interface.py
@@ -4567,7 +4567,7 @@ Please install this tool with the following MG5_aMC command:
             else:
                 preamble = misc.get_HEPTools_location_setter(
                                                  pjoin(MG5DIR,'HEPTools'),'lib')
-        preamble += "\n unset PYTHIA8DATA\n"
+        #preamble += "\n unset PYTHIA8DATA\n"
         
         open(pythia_cmd_card,'w').write("""!
 ! It is possible to run this card manually with:
