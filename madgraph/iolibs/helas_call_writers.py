f94c1422edb0b8d7843ae1c554fafc9e87a16e6c
Olivier Mattelaer <olivier.mattelaer@uclouvain.be>
2024-05-28 13:37:44 +0200

    remove debug statement

--- madgraph/iolibs/helas_call_writers.py
+++ madgraph/iolibs/helas_call_writers.py
@@ -1127,7 +1127,6 @@ class FortranUFOHelasCallWriter(UFOHelasCallWriter):
     def generate_external_wavefunction(self,argument):
         """ Generate an external wavefunction """
 
-        misc.sprint(argument['particle']['name'],argument.get('offshell')) 
         call="CALL "
         call_function = None
         if argument.get('is_loop'):
