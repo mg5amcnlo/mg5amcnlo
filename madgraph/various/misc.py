2604e3f2757b217e2b62315db00d2277b798f940
Olivier Mattelaer <olivier.mattelaer@uclouvain.be>
2024-05-28 22:27:46 +0200

    more debug statement2

--- madgraph/various/misc.py
+++ madgraph/various/misc.py
@@ -2242,19 +2242,20 @@ def import_python_lhapdf(lhapdfconfig):
                 os.environ['LD_LIBRARY_PATH'] = lhapdf_libdir
             else:
                 os.environ['LD_LIBRARY_PATH'] = '%s:%s' %(lhapdf_libdir,os.environ['LD_LIBRARY_PATH'])
-        misc.sprint(lhapdf_libdir)
-        misc.sprint(pjoin(lhapdf_libdir, os.pardir, 'local'))
-        misc.sprint(os.listdir('/home/runner/work/mg5amcnlo/mg5amcnlo/HEPTools/lhapdf6_py3/')) 
+        sprint(lhapdf_libdir)
+        sprint(pjoin(lhapdf_libdir, os.pardir, 'local'))
+        sprint(os.listdir('/home/runner/work/mg5amcnlo/mg5amcnlo/HEPTools/lhapdf6_py3/local'))
+        sprint(os.listdir(pjoin(lhapdf_libdir, os.pardir, 'local'))) 
         try:
             candidates=[dirname for dirname in os.listdir(lhapdf_libdir) \
                             if os.path.isdir(os.path.join(lhapdf_libdir,dirname))]
         except OSError:
             candidates=[]
-        misc.sprint(candidates)
+        sprint(candidates)
         if os.path.isdir(pjoin(lhapdf_libdir, os.pardir, 'local')):
             candidates += [pjoin(os.pardir,'local', dirname) for dirname in os.listdir(pjoin(lhapdf_libdir, os.pardir, 'local'))
                            if os.path.isdir(os.path.join(lhapdf_libdir,os.pardir, 'local', dirname))]
-            misc.sprint(candidates)
+            sprint(candidates)
         for candidate in candidates:
             for subdir in ['site-packages', 'dist-packages']:
                 if os.path.isdir(os.path.join(lhapdf_libdir,candidate, subdir)):
