dd1e325600e08362044d798c4c92021ecfc91a25
Olivier Mattelaer <olivier.mattelaer@uclouvain.be>
2025-05-16 09:18:15 +0200

    propose change for some weird systematics issue in multi-core

--- madgraph/interface/common_run_interface.py
+++ madgraph/interface/common_run_interface.py
@@ -1964,12 +1964,16 @@ class CommonRunCmd(HelpToCmd, CheckValidForCmd, cmd.Cmd):
                 self.cluster.wait(os.path.dirname(output), update_status, update_first=update_status)
             except Exception:
                 self.cluster.remove()
+                for i in range(nb_submit):
+                    os.remove('%s/tmp_%s_%s' %(os.path.dirname(output),i,os.path.basename(output)))
                 old_run_mode = self.options['run_mode']
                 self.options['run_mode'] =0
+                out =False
                 try:
                     out = self.do_systematics(line)
                 finally:
                     self.options['run_mode']  =  old_run_mode
+                return out
             #collect the data
             all_cross = []
             for i in range(nb_submit):
@@ -1995,27 +1999,20 @@ class CommonRunCmd(HelpToCmd, CheckValidForCmd, cmd.Cmd):
                                        self.run_card['event_norm'] in ['unity']:
                 all_cross= [cross/nb_event for cross in all_cross]
                 
-            # RR 2025 0515
-            # 'old_run_mode' only defined to bypass crash.
-            # if exists, no need to re-re-run print
-            if 'old_run_mode' in locals():
-                self.update_status('Skipping (re)run of call_systematics', level='parton', makehtml=False)
-                self.update_status('Skipping (re)run of print_cross_sections', level='parton', makehtml=False)
-                self.update_status('Skipping (re)concatenation', level='parton', makehtml=False)
-            else:
-                sys_obj = systematics.call_systematics([input, None] + opts,
-                                         log=lambda x: logger.info(str(x)),
-                                         result=result_file,
-                                         running=False
-                                         )
 
-                sys_obj.print_cross_sections(all_cross, nb_event, result_file)
+            sys_obj = systematics.call_systematics([input, None] + opts,
+                                        log=lambda x: logger.info(str(x)),
+                                        result=result_file,
+                                        running=False
+                                        )
 
-                #concatenate the output file
-                subprocess.call(['cat']+\
-                                ['./tmp_%s_%s' % (i, os.path.basename(output)) for i in range(nb_submit)],
-                                stdout=open(output,'w'),
-                                cwd=os.path.dirname(output))
+            sys_obj.print_cross_sections(all_cross, nb_event, result_file)
+
+            #concatenate the output file
+            subprocess.call(['cat']+\
+                            ['./tmp_%s_%s' % (i, os.path.basename(output)) for i in range(nb_submit)],
+                            stdout=open(output,'w'),
+                            cwd=os.path.dirname(output))
                 
             for i in range(nb_submit):
                 os.remove('%s/tmp_%s_%s' %(os.path.dirname(output),i,os.path.basename(output)))
