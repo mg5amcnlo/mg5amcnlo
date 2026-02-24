8d4bbc2d42b7aa83a04f6a60e64a4da095dcf79c
riruiz <richard.physics@gmail.com>
2026-02-24 06:16:22 +0100

    adding polarization help

--- madgraph/interface/madgraph_interface.py
+++ madgraph/interface/madgraph_interface.py
@@ -620,6 +620,11 @@ class HelpToCmd(cmd.HelpCmd):
         logger.info(" o core process, decay1, (decay2, (decay2', ...)), ...  etc")
         logger.info(" o Example: generate p p > t~ t QED=0, (t~ > W- b~, W- > l- vl~), t > j j b @2",'$MG:color:GREEN')
         logger.info(" > Note that identical particles will all be decayed.")
+        logger.info("Polarized process syntax:",'$MG:BOLD')
+        logger.info(" > Fix the helicity polarizations of external particles (massless or massive) or massive internal particles")
+        logger.info("   before decay chain by adding '{X}' to (multi)particles in process definitions.")
+        logger.info(" > Example: generate t{L} > w+{T} b{R}, w+ > ta+ vt",'$MG:color:GREEN')
+        logger.info(" > For further help, see 'help polarization'")
         logger.info("Loop processes syntax:",'$MG:BOLD')
         logger.info(" o core process [ <NLO_mode=> LoopOrder1 LoopOrder2 ... ] SQUAREDCOUPi=ORDERi")
         logger.info(" o Example: generate p p > t~ t QED=0 QCD=2 [ all= QCD ] QCD=6",'$MG:color:GREEN')
@@ -657,6 +662,11 @@ class HelpToCmd(cmd.HelpCmd):
         logger.info(" o core process, decay1, (decay2, (decay2', ...)), ...  etc")
         logger.info(" o Example: add process p p > t~ t QED=0, (t~ > W- b~, W- > l- vl~), t > j j b @2",'$MG:color:GREEN')
         logger.info(" > Note that identical particles will all be decayed.")
+        logger.info("Polarized process syntax:",'$MG:BOLD')
+        logger.info(" > Fix the helicity polarizations of external particles (massless or massive) or massive internal particles")
+        logger.info("   before decay chain by adding '{X}' to (multi)particles in process definitions.")
+        logger.info(" > Example: add process t{L} > w+{T} b{R}, w+ > ta+ vt",'$MG:color:GREEN')
+        logger.info(" > For further help, see 'help polarization'")
         logger.info("Loop processes syntax:",'$MG:BOLD')
         logger.info(" o core process [ <NLO_mode=> LoopOrder1 LoopOrder2 ... ] SQUAREDCOUPi=ORDERi")
         logger.info(" o Example: add process p p > t~ t QED=0 QCD=2 [ all= QCD ] QCD=6",'$MG:color:GREEN')
@@ -747,6 +757,22 @@ class HelpToCmd(cmd.HelpCmd):
         logger.info("Example: define p = g u u~ c c~ d d~ s s~ b b~",'$MG:color:GREEN')
         logger.info("Special syntax: Use | for OR (used for required s-channels)")
         logger.info("Special syntax: Use / to remove particles. Example: define q = p / g")
+
+    def help_polarization(self):
+        logger.info("Polarized process syntax:",'$MG:BOLD')
+        logger.info(" > Fix the helicity polarizations of external particles (massless or massive) or massive internal particles")
+        logger.info("   before decay chain by adding '{X}' to (multi)particles in process definitions.")
+        logger.info(" > Example: generate t{L} > w+{T} b{R}, w+ > ta+ vt",'$MG:color:GREEN')
+        logger.info(" > Example: generate p p > z{T} z{A}, z > e+ e-",'$MG:color:GREEN')
+        logger.info(" > Example: generate p p > z{0} z{T}, z > e+ e-, z > mu+ mu-",'$MG:color:GREEN')
+        logger.info(" > Users need to set 'group_subprocesses False', 'nhel=1' (run_card), and 'me_frame' (run_card)")
+        logger.info(" > For the proces 'p p > w+ z j j, w+ > l+ vl, z > l+ l-', the WZ rest frame is given by me_frame = [3,4,5,6]")
+        logger.info(" > For further details, see appendices of [arXiv:1912.01725] and [arXiv:2512.10015].")
+        logger.info("   For possibilities with loop-induced processes, see [2401.17365].")
+    
+    def help_polarisation(self):
+        logger.info("Polarized process syntax:",'$MG:BOLD')
+        logger.info(" > See 'help polarization'")
 
     def help_set(self):
         logger.info("-- set options for generation or output.",'$MG:color:BLUE')
