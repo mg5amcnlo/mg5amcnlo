842a6a9ce7d8e3914f32b01145efe3fa0624a0ab
Olivier Mattelaer <olivier.mattelaer@uclouvain.be>
2026-06-04 11:59:53 +0200

    update matchox check_sa reference

--- tests/input_files/IOTestsComparison/TestCmdMatchBox/MatchBoxOutput/%TEST%SubProcesses%P0_wpwm_wpwm%check_sa.f
+++ tests/input_files/IOTestsComparison/TestCmdMatchBox/MatchBoxOutput/%TEST%SubProcesses%P0_wpwm_wpwm%check_sa.f
@@ -33,7 +33,9 @@ C
       REAL*8 PIN(0:3), POUT(0:3)
       CHARACTER*120 BUFF(NEXTERNAL)
       CHARACTER*30 SQRTS_STR
+      CHARACTER*30 NB_TRY_STR, UFLAVOR_STR
       INTEGER ARGC
+      INTEGER NB_TRY, unique_flavor
       INTEGER MAXFLAVOR
       PARAMETER (MAXFLAVOR=1)
       INTEGER FLAVOR(NEXTERNAL, MAXFLAVOR)
@@ -69,12 +71,12 @@ c     RANDOM set of four momenta of given masses pmass(i) to be used to evaluate
 c     the MadGraph5_aMC@NLO matrix-element.       
 c     Alternatevely, here the user can call or set the four momenta at his will, see below.
 c     	
+      ARGC = COMMAND_ARGUMENT_COUNT()
       IF(nincoming.EQ.1) THEN
-         SQRTS=PMASS(1)
+        SQRTS=PMASS(1)
       ELSE
-         SQRTS=1000d0              !CMS energy in GEV
-c---  Allow the energy to be set via command-line argument: ./check <energy>
-         ARGC = COMMAND_ARGUMENT_COUNT()
+        SQRTS=1000d0              !CMS energy in GEV
+c---  Allow the energy to be set via command-line argument: ./check <energy> [nb_try] [flavor_idx]
          IF (ARGC .GE. 1) THEN
             CALL GET_COMMAND_ARGUMENT(1, SQRTS_STR)
             READ(SQRTS_STR,*) SQRTS
@@ -82,6 +84,16 @@ c---  Allow the energy to be set via command-line argument: ./check <energy>
          IF (SQRTS.le.2.0d0*TOTALMASS) THEN
             SQRTS = 2.0d0*TOTALMASS
          ENDIF
+      ENDIF
+      NB_TRY = 1
+      unique_flavor = 0
+      IF (ARGC .GE. 2) THEN
+        CALL GET_COMMAND_ARGUMENT(2, NB_TRY_STR)
+        READ(NB_TRY_STR,*) NB_TRY
+      ENDIF
+      IF (ARGC .GE. 3) THEN
+        CALL GET_COMMAND_ARGUMENT(3, UFLAVOR_STR)
+        READ(UFLAVOR_STR,*) unique_flavor
       ENDIF
 
       call printout()
@@ -110,7 +122,10 @@ c
 c     Now we can call the matrix element!
 c
       do I=1, MAXFLAVOR
+      IF(unique_flavor.gt.0.and.unique_flavor.ne.I) CYCLE
+      do J=1, NB_TRY
       CALL SMATRIX(P,FLAVOR(1,I), MATELEM)
+      enddo
 c
       write(*,*) "PDG", PDG_FOR_FLAVOR(:,I)
       write (*,*) "Matrix element = ", MATELEM, " GeV^",-(2*nexternal-8)	
