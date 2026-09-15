b1077c4517aff0951c01509c565c0a92d701ffb0
A.M.M. Elsayed <ahmedphysica@outlook.com>
2026-09-16 00:12:38 +0800

    added fsock.flush() before both hadd calls while writing to delphes .log

--- madgraph/interface/madevent_interface.py
+++ madgraph/interface/madevent_interface.py
@@ -5429,12 +5429,14 @@ tar -czf split_$1.tar.gz split_$1
             fsock.write('=' * 35 + '\n')
             fsock.write(' -> hadd log file ' + '\n')
             fsock.write('=' * 35 + '\n')
+            fsock.flush()
             ret = misc.call([hadd_exe, '-f', '-j', str(nb), final_root] + produced,
                             stdout=fsock, stderr=subprocess.STDOUT)
             if ret != 0:
                 # The -j (parallel) option may be unsupported by older ROOT;
                 # retry the merge serially before giving up.
                 fsock.write('\nhadd -j failed, retrying without -j\n')
+                fsock.flush()
                 ret = misc.call([hadd_exe, '-f', final_root] + produced,
                                 stdout=fsock, stderr=subprocess.STDOUT)
         if ret != 0 or not os.path.isfile(final_root):
