8c24b52ee3e7d5f9f65e6d73da8e3d596ca0ce06
Rikkert <rikkert.frederix@thep.lu.se>
2026-09-22 16:04:20 +0200

    removed the validation

--- validation/born_support_ae5422c5/dy/mc.cmd
+++
@@ -1,7 +0,0 @@
-set automatic_html_opening False --no_save
-set notification_center False --no_save
-set run_mode 2 --no_save
-set nb_core 3 --no_save
-set fastjet /export/tmp/rikkert/git/mg5amcnlo/MCcntRefactor_Sfun/validation/mcatnlo_xsections_f81ea6917/fastjet-config --no_save
-launch aMC@NLO -f -p --name=born_support_mc
-quit
--- validation/history_guard_ae5422c5/ttbar/lhapdf_fo.cmd
+++
@@ -1,8 +0,0 @@
-set automatic_html_opening False --no_save
-set notification_center False --no_save
-set run_mode 2 --no_save
-set nb_core 3 --no_save
-set fastjet /export/tmp/rikkert/git/mg5amcnlo/MCcntRefactor_Sfun/validation/mcatnlo_xsections_f81ea6917/fastjet-config --no_save
-set lhapdf /nfs/home/rikkert/LHAPDF/bin/lhapdf-config --no_save
-launch NLO -f --name=history_guard_lhapdf_fo
-quit
